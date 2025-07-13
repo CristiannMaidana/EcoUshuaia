712d33fa23d4ac1f485464e00e48867273c28e52
Cristian <cristian.mg.david@gmail.com>
2025-07-13 02:03:32 -0300

    feat(ui): actualiza texto, fondo y sombras en AppBar
    - Actualiza el texto del AppBar
    - Cambia el color de fondo del AppBar
    - Añade sombras al texto del AppBar

--- lib/ui/features/login/login_screen.dart
+++ lib/ui/features/login/login_screen.dart
@@ -10,8 +10,19 @@ class LoginScreen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Color.fromRGBO(0, 137, 50, 1),
           title: Text('EcoUshuaia',
-          style: TextStyle(color: Color(0xFF202020), fontSize: 45),),
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 46,
+              shadows: [
+                Shadow(
+                  color: Colors.black,
+                  offset: Offset(0, 1),
+                  blurRadius: 2,
+                ),],
+            ),
+          ),
         ),
         body: Center(
           child: Text('Welcome to the Login Screen!'),
