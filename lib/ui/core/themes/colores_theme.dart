f443868baad875f9cb3a2ac92981b91e7ba417b6
Cristian <cristian.mg.david@gmail.com>
2025-07-14 18:32:50 -0300

    feat(core): agregar colores de botones en colores_theme
    - Añade constantes verdeBotones, textoBotones, colorBotonesEstandar y borderBotonesEstandar en colores_theme.dart

--- lib/ui/core/themes/colores_theme.dart
+++ lib/ui/core/themes/colores_theme.dart
@@ -12,3 +12,11 @@ const Color inputBlanco = Color.fromRGBO(235, 235, 240, 1);
 const Color azulBrillante = Color(0xFF90CAF9);
 
 const Color rojoError = Color(0xFFEF9A9A);
+
+const Color verdeBotones = Color(0xFF4ad968);
+
+const Color textoBotones = Color(0xFFe6e6e6);
+
+const Color colorBotonesEstandar = Color.fromARGB(217, 22, 193, 22);
+
+const Color borderBotonesEstandar = Color.fromARGB(255, 8, 73, 10);
