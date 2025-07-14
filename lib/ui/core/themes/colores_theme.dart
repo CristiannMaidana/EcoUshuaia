de498f67ac9932c421f9226531bf2a4e2e514209
Cristian <cristian.mg.david@gmail.com>
2025-07-14 00:06:12 -0300

    feat(core): crear tema de colores en core/themes
    - Añade archivo core/themes/colores_theme.dart
    - Define constantes de color: verdeBackGround, verdeLoginContainer, sombraNegro, inputBlanco, azulBrillante y rojoError

---
+++ lib/ui/core/themes/colores_theme.dart
@@ -0,0 +1,14 @@
+//Aca van a a ir todos los colores que vaya a usar en mi app
+import 'package:flutter/material.dart';
+
+const Color verdeBackGround = Color.fromRGBO(0, 137, 50, 1);
+
+const Color verdeLoginContainer = Color.fromRGBO(48, 209, 88, 1);
+
+const Color sombraNegro = Colors.black26;
+
+const Color inputBlanco = Color.fromRGBO(235, 235, 240, 1);
+
+const Color azulBrillante = Color(0xFF90CAF9);
+
+const Color rojoError = Color(0xFFEF9A9A);
