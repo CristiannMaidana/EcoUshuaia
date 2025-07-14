43711a138317fa2507679829016601271c9899c6
Cristian <cristian.mg.david@gmail.com>
2025-07-14 16:14:55 -0300

    feat(core): crear tema de espacios estándar
    - Añade archivo espacio_theme.dart en lib/ui/core/themes
    - Define constantes espacioMediano y espacioVerticalMediano para estandarizar espacios

---
+++ lib/ui/core/themes/espacio_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//Definir los diferentes espacios
+const double espacioMediano = 26;
+
+//Definir los widgets con sus espacios
+const SizedBox espacioVerticalMediano = SizedBox(height: espacioMediano);
