8ae40087b403eb7111367af2fbe31acc3733916c
Cristian <cristian.mg.david@gmail.com>
2025-07-14 18:28:34 -0300

    feat(ui): añadir widget BotonEstandar en custom_Button.dart
    - Crea clase BotonEstandar en lib/ui/core/ui/custom_Button.dart
    - Implementa icono opcional, estilo personalizable y manejo de tamaño con SizedBox

---
+++ lib/ui/core/ui/custom_Button.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class BotonEstandar extends StatelessWidget {
+  final String texto;
+  final VoidCallback onPressed;
+  final IconData? icono;
+  //final bool expandido;
+  final ButtonStyle? style;
+  final double? width;
+  final double? height;
+
+  const BotonEstandar({
+    super.key,
+    required this.texto,
+    required this.onPressed,
+    this.icono,
+    //this.expandido = false,
+    this.style,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Widget child = icono != null
+        ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icono),
+              const SizedBox(width: 8),
+              Text(texto),
+            ],
+          )
+        : Text(texto);
+
+    Widget button = ElevatedButton(
+      onPressed: onPressed,
+      child: child,
+      style: style,
+    );
+
+    Widget result = button;
+
+    // Si pasás ancho o alto, lo envuelve en un SizedBox
+    if (width != null || height != null) {
+      result = SizedBox(
+        width: width,
+        height: height,
+        child: button,
+      );
+    }// else if (expandido) {
+     // result = SizedBox( child: button);
+   // }
+
+    return result;
+  }
+}
