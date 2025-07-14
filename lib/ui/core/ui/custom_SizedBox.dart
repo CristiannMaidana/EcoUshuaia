6e6403b8e4a2cfc426f55c36cbefd24962e9220d
Cristian <cristian.mg.david@gmail.com>
2025-07-14 18:29:37 -0300

    feat(ui): añadir espacio horizontal para botones en custom_SizedBox
    - Añade constante espacioHorizontalBotones (SizedBox(width: 80)) en custom_SizedBox.dart

--- lib/ui/core/ui/custom_SizedBox.dart
+++ lib/ui/core/ui/custom_SizedBox.dart
@@ -5,3 +5,5 @@ const double espacioMediano = 26;
 
 //Definir los widgets con sus espacios
 const SizedBox espacioVerticalMediano = SizedBox(height: espacioMediano);
+
+const SizedBox espacioHorizontalBotones = SizedBox(width: 80);
