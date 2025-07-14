c5a24e3d2c85c234057492c3c74a74b3e71c2c4c
Cristian <cristian.mg.david@gmail.com>
2025-07-14 00:53:53 -0300

    feat(core): crear tema de inputs en core/themes
    - Añade inputs_theme.dart con appInputDecorationTheme
    - Configura floatingLabelBehavior, contentPadding, fillColor y filled
    - Define OutlineInputBorder en focusedBorder, border, focusedErrorBorder y errorBorder
    - Ajusta errorMaxLines a 10

---
+++ lib/ui/core/themes/inputs_theme.dart
@@ -0,0 +1,36 @@
+// inputs_theme.dart
+import 'package:flutter/material.dart';
+import 'colores_theme.dart'; // Importá tus colores personalizados
+
+final InputDecorationTheme appInputDecorationTheme = InputDecorationTheme(
+  floatingLabelBehavior: FloatingLabelBehavior.never,
+  contentPadding: EdgeInsets.all(20),
+  fillColor: inputBlanco,
+  filled: true,
+  focusedBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(50),
+    borderSide: BorderSide(
+      color: azulBrillante,
+      width: 1.5,
+    ),
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(50),
+    borderSide: BorderSide(color: sombraNegro, width: 1),
+  ),
+  focusedErrorBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(50),
+    borderSide: BorderSide(
+      color: rojoError,
+      width: 1.5,
+    ),
+  ),
+  errorBorder: OutlineInputBorder(
+    borderRadius: BorderRadius.circular(50),
+    borderSide: BorderSide(
+      color: rojoError,
+      width: 1.5,
+    ),
+  ),
+  errorMaxLines: 10,
+);
