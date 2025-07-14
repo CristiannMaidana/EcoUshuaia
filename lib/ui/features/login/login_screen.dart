e48de1bf1e9fa7f51e111503624580e1e076d2da
Cristian <cristian.mg.david@gmail.com>
2025-07-14 16:07:20 -0300

    feat(login): aplicar container_decoration_theme en LoginScreen
    - Importa container_decoration_theme.dart en login_screen.dart
    - Usa containerInputsLogin en la propiedad decoration del Container

--- lib/ui/features/login/login_screen.dart
+++ lib/ui/features/login/login_screen.dart
@@ -1,7 +1,7 @@
-import 'package:eco_ushuaia/ui/core/themes/colores_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:eco_ushuaia/utils/validators_login.dart';
 import 'package:eco_ushuaia/ui/core/themes/login_theme.dart';
+import 'package:eco_ushuaia/ui/core/themes/container_decoration_theme.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,41 +15,25 @@ class LoginScreen extends StatefulWidget{
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-
 class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
         appBar: AppBar(title: Text('EcoUshuaia', style: Theme.of(context).textTheme.displayLarge),),
         body: Container(
           alignment: Alignment.center,
           padding: EdgeInsets.all(25), // Add padding to the body
           
           child: Container(
-            decoration: BoxDecoration(
-              color: verdeLoginContainer,
-              borderRadius: BorderRadius.circular(50), // Rounded corners
-              border: Border.all(
-                color: Color.fromRGBO(10, 20, 13, 0.498),
-                width: 1, // Border width
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: sombraNegro,
-                  offset: Offset(0, 7),
-                  blurRadius: 20,
-                ),
-              ],
-            ),
+            decoration: containerInputsLogin,
             width: 600,
             height: 600,            
             padding:EdgeInsets.all(25),// Add padding to the container
-            
             child: ListView(
               children: <Widget>[
-                SizedBox(height: 4), // Add space at the top
+                SizedBox(height: 26), // Add space at the top
                 Text('Ingrese en su cuenta', style: Theme.of(context).textTheme.headlineLarge),
                 SizedBox(height: 26), // Add space between text and text fields
                 // Form for email 
