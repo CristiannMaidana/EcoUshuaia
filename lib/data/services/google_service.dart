71bee2dbb899c34985c702f733478ee36a9cc986
Cristian <cristian.mg.david@gmail.com>
2025-07-15 00:50:29 -0300

    feat(auth): crear servicio Google Sign-In
    - Crea carpeta data/services y archivo google_service.dart
    - Añade placeholder para métodos de autenticación Gmail
    - Importa paquete google_sign_in

---
+++ lib/data/services/google_service.dart
@@ -0,0 +1,5 @@
+//Aca tiene que ir los metodos para la autenficiacion de gmail,
+//lo unico que se hizo fue instalar y crear las carpetas para google_sign_in
+//es lo unico que se toco y avanzo.
+
+//import 'package:google_sign_in/google_sign_in.dart';
