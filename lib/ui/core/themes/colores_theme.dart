0410e69b24bed04b60221ca768dd65f4b95755eb
Cristian <cristian.mg.david@gmail.com>
2025-07-16 20:19:25 -0300

    feat(core): agregar gama de colores verdes en colores_theme
    - Añade colorNegro y paleta de tonalidades verdes camarone50–camarone950 en colores_theme.dart

--- lib/ui/core/themes/colores_theme.dart
+++ lib/ui/core/themes/colores_theme.dart
@@ -1,22 +1,36 @@
 //Aca van a a ir todos los colores que vaya a usar en mi app
 import 'package:flutter/material.dart';
 
-const Color verdeBackGround = Color.fromRGBO(0, 137, 50, 1);
-
-const Color verdeLoginContainer = Color.fromRGBO(48, 209, 88, 1);
-
 const Color sombraNegro = Colors.black26;
-
-const Color inputBlanco = Color.fromRGBO(235, 235, 240, 1);
 
 const Color azulBrillante = Color(0xFF90CAF9);
 
 const Color rojoError = Color(0xFFEF9A9A);
 
-const Color verdeBotones = Color(0xFF4ad968);
+const Color colorNegro = Colors.black;
 
-const Color textoBotones = Color(0xFFe6e6e6);
+//Colores finales 
+const Color camarone50  = Color.fromRGBO(242, 255, 240, 1);
 
-const Color colorBotonesEstandar = Color.fromARGB(217, 22, 193, 22);
+const Color camarone100 = Color.fromRGBO(217, 255, 214, 1);
 
-const Color borderBotonesEstandar = Color.fromARGB(255, 8, 73, 10);
+const Color camarone200 = Color.fromRGBO(179, 255, 179, 1);
+
+const Color camarone300 = Color.fromRGBO(112, 255, 119, 1);
+
+const Color camarone400 = Color.fromRGBO(46, 250, 67, 1);
+
+const Color camarone500 = Color.fromRGBO(3, 226, 37, 1);
+
+const Color camarone600 = Color.fromRGBO(0, 189, 35, 1);
+
+const Color camarone700 = Color.fromRGBO(1, 147, 37, 1);
+
+const Color camarone800 = Color.fromRGBO(7, 116, 39, 1);
+
+const Color camarone900 = Color.fromRGBO(8, 94, 41, 1);
+
+//Para los textos
+const Color camarone950 = Color.fromRGBO(0, 56, 23, 1);
+//Gradiente fondo 200 y 300
+//Boton borde 900, gradiente (700,800)
