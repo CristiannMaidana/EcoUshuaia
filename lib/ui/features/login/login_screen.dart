88b2057dfee46b2a9a8bf75542fac7158f02d604
Cristian <cristian.mg.david@gmail.com>
2025-07-28 20:31:26 -0300

    refactor(login): actualizar rutas de imports en LoginScreen
    - Importa custom_avatar y custom_eye_password desde custom_lottie
    - Importa validators_login desde utils/validator_login

--- lib/ui/features/login/login_screen.dart
+++ lib/ui/features/login/login_screen.dart
@@ -1,12 +1,12 @@
 import 'package:eco_ushuaia/ui/core/ui/custom_Button.dart';
-import 'package:eco_ushuaia/ui/core/ui/custom_eye_password.dart';
+import 'package:eco_ushuaia/ui/core/ui/custom_lottie/custom_eye_password.dart';
 import 'package:eco_ushuaia/ui/features/forgot_password/forgot_password_screen.dart';
 import 'package:eco_ushuaia/ui/features/register_screen/register_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:eco_ushuaia/utils/validators_login.dart';
+import 'package:eco_ushuaia/utils/validator_login/validators_login.dart';
 import 'package:eco_ushuaia/ui/core/themes/container_decoration_theme.dart';
 import 'package:eco_ushuaia/ui/core/ui/custom_SizedBox.dart';
-import 'package:eco_ushuaia/ui/core/ui/custom_avatar.dart';
+import 'package:eco_ushuaia/ui/core/ui/custom_lottie/custom_avatar.dart';
 
 class LoginScreen extends StatefulWidget{
   @override
