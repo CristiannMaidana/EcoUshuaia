bcc480077ec98a56563d200e773ea9705ccfc372
Cristian <cristian.mg.david@gmail.com>
2025-07-28 20:42:44 -0300

    refactor(register): convertir pantalla de registro a StatefulWidget y actualizar AppBar
    - Convierte RegisterScreen de StatelessWidget a StatefulWidget
    - Actualiza texto de AppBar a 'Crear cuenta' usando displayLarge

--- lib/ui/features/register_screen/register_screen.dart
+++ lib/ui/features/register_screen/register_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:eco_ushuaia/ui/core/themes/container_decoration_theme.dart';
 
-class RegisterScreen extends StatelessWidget{
-  const RegisterScreen({Key? key}) : super(key: key);
+class RegisterScreen extends StatefulWidget{
+  RegisterScreen({Key? key}) : super(key: key);
 
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('EcoUshuaia'), titleTextStyle: Theme.of(context).textTheme.displayLarge,),
+      appBar: AppBar(
+        title: Text('Crear cuenta'),
+        titleTextStyle: Theme.of(context).textTheme.displayLarge,
+      ),
       body: Container(
         margin: EdgeInsets.all(20),
         padding: EdgeInsets.all(25),
