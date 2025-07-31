a2a07226093195a610126fbc619006e4739bb660
Cristian <cristian.mg.david@gmail.com>
2025-07-31 19:01:28 -0300

    feat(custom_notification): Crear manejo de estados de controlador-m- Modificar Widget Lottie.assets a GestureDetector-m- Crear estados para el controlador

--- lib/ui/core/ui/custom_lottie/custom_notification.dart
+++ lib/ui/core/ui/custom_lottie/custom_notification.dart
@@ -1,30 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class CustomNotification extends StatefulWidget{
+class CustomNotification extends StatefulWidget {
   final double size;
-  final FocusNode focusNode;
 
   const CustomNotification({
     Key? key,
     this.size = 70,
-    required this.focusNode,
   }) : super(key: key);
 
   @override
-  State <CustomNotification> createState() => _CustomEmailState();
+  State<CustomNotification> createState() => _CustomNotificationState();
 }
 
-class _CustomEmailState extends State<CustomNotification> with SingleTickerProviderStateMixin{
+class _CustomNotificationState extends State<CustomNotification> with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
+  bool _touched = false;
 
   @override
-  Widget build (contexto){
-    return Lottie.asset(
-      'assets/lottie/notification_reveal.json',
-      width: widget.size,
-      height: widget.size,
-      repeat: false,
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTap() {
+    setState(() {
+      _touched = true;
+    });
+    _controller.forward(from: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Lottie.asset(
+        _touched
+            ? 'assets/lottie/notification_hover_ring.json'
+            : 'assets/lottie/notification_reveal.json',
+        controller: _controller,
+        width: widget.size,
+        height: widget.size,
+        repeat: false,
+        onLoaded: (composition) {
+          _controller.duration = composition.duration;
+          _controller.forward(from: 0);
+        },
+      ),
     );
   }
 }
