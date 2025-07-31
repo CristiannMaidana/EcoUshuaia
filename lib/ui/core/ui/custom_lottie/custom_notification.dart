638737620de9c0f758d9f9770002d6954991da82
Cristian <cristian.mg.david@gmail.com>
2025-07-31 18:38:38 -0300

    feat(custom_notification): añade el estado y la construccion del Widget-m- Crear _CustomEmailState para CustomNotification-m- Añadir el nuevo estado a CustomNotification-m- Crear un Widget que sea un Lottie

--- lib/ui/core/ui/custom_lottie/custom_notification.dart
+++ lib/ui/core/ui/custom_lottie/custom_notification.dart
@@ -10,4 +10,21 @@ class CustomNotification extends StatefulWidget{
     this.size = 70,
     required this.focusNode,
   }) : super(key: key);
+
+  @override
+  State <CustomNotification> createState() => _CustomEmailState();
+}
+
+class _CustomEmailState extends State<CustomNotification> with SingleTickerProviderStateMixin{
+  late final AnimationController _controller;
+
+  @override
+  Widget build (contexto){
+    return Lottie.asset(
+      'assets/lottie/notification_reveal.json',
+      width: widget.size,
+      height: widget.size,
+      repeat: false,
+    );
+  }
 }
