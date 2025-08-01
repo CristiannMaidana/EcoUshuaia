12a2eaf33c915c72ca8b3d74b22ab71432b5ea6a
Cristian <cristian.mg.david@gmail.com>
2025-07-31 21:55:46 -0300

    feat(home_screen): Crear AppBar-m- Crear un AppBar-m- Añadir CustomNotification-m- Añadir una lista que simula las notificaciones para testeo

--- lib/ui/features/home/home_screen.dart
+++ lib/ui/features/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eco_ushuaia/ui/core/ui/custom_lottie/custom_notification.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget{
@@ -8,10 +9,16 @@ class HomeScreen extends StatefulWidget{
 }
 
 class _HomeScreenState extends State<HomeScreen>{
+  final List listaNotificaciones=List.empty(); //Simula ser la lista de todas las notificaciones de la base de dato
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      
+      appBar: AppBar(
+        actions: [
+          CustomNotification(notificaciones: listaNotificaciones)
+        ],
+      ),
     );
   }
 }
