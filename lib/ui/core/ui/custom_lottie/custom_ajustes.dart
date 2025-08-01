3fe9373958c06831909bd9a3c0ce02bbf4f62ee9
Cristian <cristian.mg.david@gmail.com>
2025-08-01 17:52:54 -0300

    feat(custom_ajustes): Crear hijo Lottie a GestureDetector-m- Crear opcion de lotties settings-m- Añadir alto y ancho al lottie

--- lib/ui/core/ui/custom_lottie/custom_ajustes.dart
+++ lib/ui/core/ui/custom_lottie/custom_ajustes.dart
@@ -25,6 +25,11 @@ class _CustomAjustesState extends State<CustomAjustes> with SingleTickerProvider
   Widget build(context){
     return GestureDetector(
       onTap: _handleTap,
+      child: Lottie.asset(
+        _touched ? 'assets/lottie/settings_hover_pinch.json' : 'assets/lottie/settings_in_reveal.json',
+        width: widget.size,
+        height: widget.size,
+      ),
     );
   }
 }
