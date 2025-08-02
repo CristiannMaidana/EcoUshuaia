69405f71b1c344ec8e1bca90662e27f501f720f3
Cristian <cristian.mg.david@gmail.com>
2025-08-02 15:25:09 -0300

    feat(settings_screen): agregar theme texto-m- Agregar Theme (displayLarge) a AppBar

--- lib/ui/features/home_pages/settings/settings_screen.dart
+++ lib/ui/features/home_pages/settings/settings_screen.dart
@@ -5,7 +5,7 @@ class SettingsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Configuracion')),
+      appBar: AppBar(title: Text('Configuracion', style: Theme.of(context).textTheme.displayLarge,)),
       body: Center(
         child: Column(
           
