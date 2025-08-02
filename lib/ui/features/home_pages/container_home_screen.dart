53c40feb789feefd48dc92309c00c0c7a9004e4a
Cristian <cristian.mg.david@gmail.com>
2025-08-02 15:17:46 -0300

    refactor(container_home_screen): borrar AppBar-m- Borarr AppBar-m- Añadir home_screen-m- Añadir clase HomeScreen en _pages

--- lib/ui/features/home_pages/container_home_screen.dart
+++ lib/ui/features/home_pages/container_home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:eco_ushuaia/ui/core/ui/custom_ButtomNavegationBar.dart';
-import 'package:eco_ushuaia/ui/core/ui/custom_lottie/custom_notification.dart';
+import 'package:eco_ushuaia/ui/features/home_pages/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class ContainerHomeScreen extends StatefulWidget{
@@ -10,11 +10,10 @@ class ContainerHomeScreen extends StatefulWidget{
 }
 
 class _ContainerHomeScreenState extends State<ContainerHomeScreen>{
-  final List listaNotificaciones=List.empty(); //Simula ser la lista de todas las notificaciones de la base de dato
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
-    Center(child: Text('Inicio')),
+    HomeScreen(),
     Center(child: Text('Calendario')),
     Center(child: Text('Mapa')),
     Center(child: Text('Ajustes'),)
@@ -23,23 +22,6 @@ class _ContainerHomeScreenState extends State<ContainerHomeScreen>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Container(
-            padding: EdgeInsets.only(right: 20),
-            child:  Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.white,
-                border: Border.all(
-                  width: 1
-                )
-              ),
-              child: CustomNotification(notificaciones: listaNotificaciones)
-              ),
-          )
-        ],
-      ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: CustomBottomNavBar(
         selectedIndex: _selectedIndex,
