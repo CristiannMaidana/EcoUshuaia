bd7422cd3d26cd0a2786cba6a6899d2c8b41f25e
Cristian <cristian.mg.david@gmail.com>
2025-08-02 16:15:21 -0300

    feat(settings_screen): crear estructura de GestureDetector-m- Crear GestureDetector

--- lib/ui/features/home_pages/settings/settings_screen.dart
+++ lib/ui/features/home_pages/settings/settings_screen.dart
@@ -8,7 +8,26 @@ class SettingsScreen extends StatelessWidget{
       appBar: AppBar(title: Text('Configuracion', style: Theme.of(context).textTheme.displayLarge,)),
       body: Center(
         child: Column(
-          
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Cuenta'),
+            Container(
+              child: Column(
+                children: [
+                  GestureDetector(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text('Editar perfil'),
+                        Icon(Icons.insert_chart_outlined_rounded)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
