9c4aed946a0ba3b0dc4ef134af29210f1a6bcb55
Cristian <cristian.mg.david@gmail.com>
2025-08-03 18:53:20 -0300

    feat(custom_sun_nigh): crear funcionalidades de animacion-m- Crear dispose-m- Crear initState-m- Crear didUpdateWidget-m- Crea controller y modoNoche a constructor

--- lib/ui/core/ui/custom_lottie/custom_sun_night.dart
+++ lib/ui/core/ui/custom_lottie/custom_sun_night.dart
@@ -3,10 +3,12 @@ import 'package:lottie/lottie.dart';
 
 class CustomSunNight extends StatefulWidget{
   final double size;
+  final bool modoNoche;
 
   const CustomSunNight({
     Key ? key,
     this.size = 36,
+    required this.modoNoche,
   }): super (key: key);
 
   @override
@@ -14,6 +16,34 @@ class CustomSunNight extends StatefulWidget{
 }
 
 class _CustomSunNightState extends State<CustomSunNight> with SingleTickerProviderStateMixin{
+  late final AnimationController _controller;
+
+  @override
+  void didUpdateWidget(covariant CustomSunNight oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.modoNoche != widget.modoNoche) {
+      if (widget.modoNoche) {
+        _controller.forward(from: 0);
+      } else {
+        _controller.reverse(from: 1);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _controller.value = widget.modoNoche ? 1 : 0;
+    });
+    }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(context) {
@@ -22,7 +52,11 @@ class _CustomSunNightState extends State<CustomSunNight> with SingleTickerProvid
         'assets/lottie/sun_night.json',
         height: widget.size,
         width: widget.size,
-        animate: false
+        animate: false,
+        controller: _controller,
+        onLoaded: (composition) {
+          _controller.duration = composition.duration;
+        } 
       ),
     );
   }
