a2ea12ab140df48e27808bf9f01e46abb71ff858
Cristian <cristian.mg.david@gmail.com>
2025-08-03 17:24:16 -0300

    feat(settings_screen): agregar íconos y mejorar estilo de SettingsScreen
    • Importa colores_theme y custom_SizedBox para mantener consistencia de diseño
    - Ajusta backgroundColor a camarone50 en Scaffold
    - Añade listaUsuarioIcons y listaSistemaIcons para mostrar iconos en SeccionAjustes
    - Envuelve contenido en SingleChildScrollView y utiliza espacioVerticalMediano para separación
    - Organiza SeccionAjustes dentro de un Container con margen uniforme

--- lib/ui/features/home_pages/settings/settings_screen.dart
+++ lib/ui/features/home_pages/settings/settings_screen.dart
@@ -1,3 +1,5 @@
+import 'package:eco_ushuaia/ui/core/themes/colores_theme.dart';
+import 'package:eco_ushuaia/ui/core/ui/custom_SizedBox.dart';
 import 'package:eco_ushuaia/ui/core/ui/custom_seccion_ajustes.dart';
 import 'package:eco_ushuaia/ui/features/home_pages/settings/edit_user/edit_user_screen.dart';
 import 'package:flutter/material.dart';
@@ -10,11 +12,12 @@ class SettingsScreen extends StatelessWidget{
   ];
 
   final List<String> labelsSistema = const [
+    'Notificaciones',
     'Modo oscuro',
     'Lenguaje',
     'Limpiar cache',
     'Termino de uso',
-    'Politicas de privacidad'
+    'Politicas de privacidad',
     'Eliminar cuenta',
   ];
   
@@ -25,26 +28,55 @@ class SettingsScreen extends StatelessWidget{
     EditUserScreen(),
   ];
 
+  final List<Image> listaUsuarioIcons = [
+    Image.asset('assets/icons/settings/user/user.png',),
+    Image.asset('assets/icons/settings/user/lock.png'),
+    Image.asset('assets/icons/settings/user/adress.png'),
+  ];
+
+  final List<Image> listaSistemaIcons = [
+    Image.asset('assets/icons/settings/system/bell.png'),
+    Image.asset('assets/icons/settings/system/bell.png'),
+    Image.asset('assets/icons/settings/system/language.png'),
+    Image.asset('assets/icons/settings/system/data-cleaning.png'),
+    Image.asset('assets/icons/settings/system/info.png'),
+    Image.asset('assets/icons/settings/system/file.png'),
+    Image.asset('assets/icons/settings/system/delete-user.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: camarone50,
       appBar: AppBar(title: Text('Configuracion', style: Theme.of(context).textTheme.displayLarge,)),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SeccionAjustes(
-              titulo: Text('Datos de ususario', style: Theme.of(context).textTheme.headlineLarge,), 
-              lista: labelsUsuario, 
-              listPaginas: listaPaginas
-            ),
-            SeccionAjustes(
-              titulo: Text('Sistema'),
-              lista: labelsSistema,
-              listPaginas: listaPaginas
-            ),
-          ]
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsetsGeometry.all(20),
+                child: Column(
+                  children: [
+                    SeccionAjustes(
+                      titulo: Text('Datos de ususario', style: Theme.of(context).textTheme.headlineLarge,), 
+                      lista: labelsUsuario, 
+                      listPaginas: listaPaginas,
+                      listaIcons: listaUsuarioIcons,
+                    ),
+                    espacioVerticalMediano,
+                    SeccionAjustes(
+                      titulo: Text('Sistema', style: Theme.of(context).textTheme.headlineLarge,),
+                      lista: labelsSistema,
+                      listPaginas: listaPaginas,
+                      listaIcons: listaSistemaIcons,
+                    ),
+                  ],
+                ),
+              ),
+            ]
+          ),
         ),
       ),
     );
