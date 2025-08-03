83afbe8a82fe382156844ad019bb71c68adac450
Cristian <cristian.mg.david@gmail.com>
2025-08-03 12:04:56 -0300

    refactor(custom_seccion_ajustes): modificar espacios-m- Modificar los espacios de sangria

--- lib/ui/core/ui/custom_seccion_ajustes.dart
+++ lib/ui/core/ui/custom_seccion_ajustes.dart
@@ -22,33 +22,33 @@ class _SeccionAjustesState extends State<SeccionAjustes> with SingleTickerProvid
   @override
   Widget build(context) {
     return Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            widget.titulo,
-            ...List.generate(widget.lista.length, (index) {return Container(
-                margin: EdgeInsets.symmetric(horizontal: 50),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),  
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => widget.listPaginas[index]),
-                    );
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(widget.lista[index], style: Theme.of(context).textTheme.labelLarge,),
-                      CustomChevron(),
-                    ],
-                  ),
-                ),
-              );
-            }),
-          ]
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        widget.titulo,
+        ...List.generate(widget.lista.length, (index) {return Container(
+            margin: EdgeInsets.symmetric(horizontal: 50),
+            decoration: BoxDecoration(
+              color: Colors.white,
+            ),  
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => widget.listPaginas[index]),
+                );
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(widget.lista[index], style: Theme.of(context).textTheme.labelLarge,),
+                  CustomChevron(),
+                ],
+              ),
+            ),
+          );
+        }),
+      ]
     );
   }
 }
