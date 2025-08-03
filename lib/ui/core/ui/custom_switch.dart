b76436fcd07dcbda60752d6127f78376c5de173e
Cristian <cristian.mg.david@gmail.com>
2025-08-03 20:00:19 -0300

    feat(custom_switch): agregar propiedad-m- Agregar propiedad onChanged al constructor

--- lib/ui/core/ui/custom_switch.dart
+++ lib/ui/core/ui/custom_switch.dart
@@ -2,14 +2,18 @@ import 'package:eco_ushuaia/ui/core/themes/colores_theme.dart';
 import 'package:flutter/material.dart';
 
 class CustomSwitch extends StatefulWidget {
-  const CustomSwitch({super.key});
+  final ValueChanged<bool> ? onChanged;
+  const CustomSwitch({
+    super.key, 
+    this.onChanged
+  });
 
   @override
   State<CustomSwitch> createState() => _CustomSwitchState();
 }
 
 class _CustomSwitchState extends State<CustomSwitch> {
-  bool habilitado = true;
+  bool habilitado = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +27,7 @@ class _CustomSwitchState extends State<CustomSwitch> {
         setState(() {
           habilitado = value;
         });
+        widget.onChanged?.call(value);
       },
     );
   }
