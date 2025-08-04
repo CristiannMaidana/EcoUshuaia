f939521697e9b3735e81454875a2a44f93093184
Cristian <cristian.mg.david@gmail.com>
2025-08-04 20:17:16 -0300

    feat(calender_screen): cambiar AppBar-m- Crear boton-m- Crear texto

--- lib/ui/features/home_pages/calender/calender_screen.dart
+++ lib/ui/features/home_pages/calender/calender_screen.dart
@@ -1,3 +1,5 @@
+import 'package:eco_ushuaia/ui/core/themes/colores_theme.dart';
+import 'package:eco_ushuaia/ui/core/ui/custom_Button.dart';
 import 'package:eco_ushuaia/ui/core/ui/custom_calendario.dart';
 import 'package:flutter/material.dart';
 
@@ -10,8 +12,29 @@ class _CalenderScreenState extends State<CalenderScreen> with SingleTickerProvid
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: camarone50,
       appBar: AppBar(
-        title: Text('Calendario', style: Theme.of(context).textTheme.displayLarge),
+        backgroundColor: camarone50,
+        toolbarHeight: 80,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text('Calendario', style: Theme.of(context).textTheme.bodyLarge),
+              Text('12/32/32: título ', style: Theme.of(context).textTheme.labelMedium),
+            ],
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            child: BotonEstandar(
+              texto: 'Recordatorio',
+              onPressed: () {},
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
