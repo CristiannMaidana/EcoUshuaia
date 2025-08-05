e4bf3127fcdb0a04fe8b2f59de71dfd7666f50a8
Cristian <cristian.mg.david@gmail.com>
2025-08-05 19:19:31 -0300

    feat(custom_novedades): crear metodos y variables de controller-m- Crear void initState(), void _scrollListener(), _subirTodo(), _bajarSheet() y voiddispose()

--- lib/ui/core/ui/custom_novedades.dart
+++ lib/ui/core/ui/custom_novedades.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomNovedades extends StatefulWidget{
+class CustomNovedades extends StatefulWidget {
   const CustomNovedades({Key? key}) : super(key: key);
 
   @override
@@ -8,12 +8,60 @@ class CustomNovedades extends StatefulWidget{
 }
 
 class _CustomNovedadesState extends State<CustomNovedades> {
+  bool _mostrarFlecha = false;
+  ScrollController? _localController;
+  late DraggableScrollableController _draggableController;
+
+  @override
+  void initState() {
+    super.initState();
+    _draggableController = DraggableScrollableController();
+  }
+
+  void _scrollListener() {
+    if (_localController == null) return;
+    if (_localController!.offset > 100 && !_mostrarFlecha) {
+      setState(() {
+        _mostrarFlecha = true;
+      });
+    } else if (_localController!.offset <= 100 && _mostrarFlecha) {
+      setState(() {
+        _mostrarFlecha = false;
+      });
+    }
+  }
+
+  void _subirTodo() {
+    if (_localController != null) {
+      _localController!.animateTo(
+        0,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeOut,
+      );
+    }
+  }
+
+  void _bajarSheet() {
+    // Esto mueve la sheet al tamaño inicial (ejemplo: 0.4)
+    _draggableController.animateTo(
+      0.4,
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _localController?.removeListener(_scrollListener);
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
-      initialChildSize: 0.4,   
-      minChildSize: 0.4,       
-      maxChildSize: 0.7,        
+      initialChildSize: 0.4,
+      minChildSize: 0.4,
+      maxChildSize: 0.7,
       builder: (context, scrollController) {
         return Container(
           decoration: BoxDecoration(
@@ -30,7 +78,7 @@ class _CustomNovedadesState extends State<CustomNovedades> {
               Center(
                 child: Container(
                   margin: EdgeInsets.only(top: 12, bottom: 12),
-                  width: 36,
+                  width: 40,
                   height: 5,
                   decoration: BoxDecoration(
                     color: Colors.grey[400],
