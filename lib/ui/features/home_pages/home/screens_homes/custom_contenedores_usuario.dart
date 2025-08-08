1e6bb4af693ac7485c7a7841dd70fbad57fcf6bc
Cristian <cristian.mg.david@gmail.com>
2025-08-08 13:00:16 -0300

    feat(custom_contenedores_ususario): crear clase donde va el mapa-m- Crear la estructura visual y el tamaño inicial del mapa

---
+++ lib/ui/features/home_pages/home/screens_homes/custom_contenedores_usuario.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomContenedoresUsuario extends StatefulWidget {
+  
+  @override
+  State<CustomContenedoresUsuario> createState() => _CustomContenedoresUsuarioState();
+}
+
+class _CustomContenedoresUsuarioState extends State<CustomContenedoresUsuario> with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+          child: Text('Contenedores cercanos', style: Theme.of(context).textTheme.headlineLarge),
+        ),
+        Container(
+          height: 300,
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(36),
+          border: Border.all(width: 1.5, color: Colors.grey[400]!),
+          ),
+        ),
+        FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.grey[300],
+          foregroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),),
+          child: Text('Filtrar', style: Theme.of(context).textTheme.labelLarge),
+        ),
+      ],
+    );
+  }
+}
