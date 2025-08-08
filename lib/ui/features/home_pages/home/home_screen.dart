4cd9e0ccfbe6954034ab656ca3331aaff7e310f2
Cristian <cristian.mg.david@gmail.com>
2025-08-08 13:02:26 -0300

    feat(home_screen): agregar paginas-m- Crear color de fondo a la pagina-m- Agregar las ventanas y las importaciones-m- Agregar (CustomDatosUsuarios, CustomContenedoresUsuarios, CustomMAterialesInfo, CustomNovedadesHome)

--- lib/ui/features/home_pages/home/home_screen.dart
+++ lib/ui/features/home_pages/home/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:eco_ushuaia/ui/core/themes/colores_theme.dart';
 import 'package:eco_ushuaia/ui/core/ui/custom_lottie/custom_notification.dart';
+import 'package:eco_ushuaia/ui/features/home_pages/home/screens_homes/custom_contenedores_usuario.dart';
+import 'package:eco_ushuaia/ui/features/home_pages/home/screens_homes/custom_datos_ususarios_screen.dart';
+import 'package:eco_ushuaia/ui/features/home_pages/home/screens_homes/custom_materiales_info.dart';
+import 'package:eco_ushuaia/ui/features/home_pages/home/screens_homes/custom_novedades_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget{
@@ -17,7 +22,10 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   @override
   Widget build(context){
     return Scaffold(
+      backgroundColor: camarone50,
       appBar: AppBar(
+        backgroundColor: camarone50,
+        title: const Text('Eco Ushuaia', style: TextStyle(color: Colors.black)),
         actions: [
           Container(
             padding: EdgeInsets.only(right: 20),
@@ -34,6 +42,17 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
           )
         ],
       ),    
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomDatosUsuario(),
+            CustomContenedoresUsuario(),
+            CustomMaterialesInfo(),
+            CustomNovedadesHome(),
+          ],
+        ),
+      )
     );
   }
 }
