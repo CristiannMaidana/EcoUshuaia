d02ca85f0dc5365bba5c7941bcb9c9fd9ee77406
Cristian <cristian.mg.david@gmail.com>
2025-08-08 20:05:41 -0300

    feat(container_mapa_screen): crear clase-m- Crear esqueleto de la clase

---
+++ lib/ui/features/home_pages/mapa/container_mapa_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MapaScreen extends StatefulWidget{
+
+  @override
+  State<MapaScreen> createState() => _MapaScreenState();
+}
+
+class _MapaScreenState extends State<MapaScreen> with SingleTickerProviderStateMixin {
+  @override
+  Widget build(context){
+    return Scaffold();
+  }
+}
