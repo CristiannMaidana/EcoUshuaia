8a2ffe5e12a56bb9e602695c87e6d68ae6902de6
Cristian <cristian.mg.david@gmail.com>
2025-08-09 13:12:37 -0300

    feat(custom_lista_contenedores): crear clase con StatefulWidget-m- Crear estructura base de clase-m- Esta clase sera para generar los contenedores y enviar al seleccionado

---
+++ lib/ui/core/ui/custom_lista_contenedores.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomListaContenedores extends StatefulWidget{
+
+  CustomListaContenedores({
+    Key ? key,
+  }): super (key: key);
+
+  @override
+  State<CustomListaContenedores> createState() => _CustomListaContenedoresState();
+}
+
+class _CustomListaContenedoresState extends State<CustomListaContenedores> with SingleTickerProviderStateMixin{
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
