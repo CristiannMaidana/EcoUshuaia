2eb9cfee423a054c56a4166a213bbbc5487892e4
Cristian <cristian.mg.david@gmail.com>
2025-08-08 23:32:06 -0300

    feat(container_home_screen): crear AppBar-m- Crear TabBar-m- Crear color al fondo

--- lib/ui/features/home_pages/mapa/container_mapa_screen.dart
+++ lib/ui/features/home_pages/mapa/container_mapa_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eco_ushuaia/ui/core/themes/colores_theme.dart';
 import 'package:flutter/material.dart';
 
 class MapaScreen extends StatefulWidget{
@@ -9,6 +10,31 @@ class MapaScreen extends StatefulWidget{
 class _MapaScreenState extends State<MapaScreen> with SingleTickerProviderStateMixin {
   @override
   Widget build(context){
-    return Scaffold();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: camarone50,
+        appBar: AppBar(
+          backgroundColor: camarone400,
+          toolbarHeight: 0,
+          bottom: TabBar(
+            indicatorSize: TabBarIndicatorSize.tab,
+            unselectedLabelColor: Colors.grey[600],
+            isScrollable: false,
+            labelStyle: Theme.of(context).textTheme.labelLarge,
+            tabs: const [
+              Tab(text: 'Contenedores'),
+              Tab(text: 'Mapa'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text('Contenedores')),
+            Center(child: Text('Mapa')),
+          ],
+        ),
+      ),
+    );
   }
 }
