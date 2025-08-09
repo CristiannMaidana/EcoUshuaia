80abb9f987aeeef73cec58b98fdf85e1abc63246
Cristian <cristian.mg.david@gmail.com>
2025-08-09 19:44:47 -0300

    feat(mapa_screen): añadir CustomMapa-m- Crear un container para el mapa

--- lib/ui/features/home_pages/mapa/screens_mapa/mapa_screen.dart
+++ lib/ui/features/home_pages/mapa/screens_mapa/mapa_screen.dart
@@ -1,3 +1,4 @@
+import 'package:eco_ushuaia/ui/core/ui/custom_mapa.dart';
 import 'package:flutter/material.dart';
 
 class MapaScreen extends StatefulWidget{
@@ -14,6 +15,14 @@ class _MapaScreenState extends State<MapaScreen> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context){
-    return Center();
+    return Center(
+      child: Container(
+        height: 500,
+        decoration: BoxDecoration(
+          border: Border.all(width: 1)
+        ),
+        child: CustomMapa()
+      ),
+    );
   }
 }
