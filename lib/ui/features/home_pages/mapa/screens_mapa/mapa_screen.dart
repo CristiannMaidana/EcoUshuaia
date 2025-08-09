21096aaa09899ed6a1d288b0804befa832818071
Cristian <cristian.mg.david@gmail.com>
2025-08-09 18:58:59 -0300

    feat(mapa_screen): crear nueva clase-m- Crear estructura base de mapa_screen

---
+++ lib/ui/features/home_pages/mapa/screens_mapa/mapa_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MapaScreen extends StatefulWidget{
+
+  MapaScreen({
+    Key? key,
+  }): super(key: key);
+
+  @override
+  State<MapaScreen> createState() => _MapaScreenState();
+}
+
+class _MapaScreenState extends State<MapaScreen> with SingleTickerProviderStateMixin{
+
+  @override
+  Widget build(BuildContext context){
+    return Center();
+  }
+}
