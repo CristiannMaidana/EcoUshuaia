285f2fd1e212cadf6794e7a7ca83460e814a302e
Cristian <cristian.mg.david@gmail.com>
2025-08-09 12:21:25 -0300

    feat(custom_Pop_up_menu_botton): crear itemBuilder y constructor-m- Crear propiedad categorias en constructor -m- Crear itemBuilder con List.generate-m- Crear onSelected

--- lib/ui/core/ui/custom_Pop_up_menu_botton.dart
+++ lib/ui/core/ui/custom_Pop_up_menu_botton.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CustomPopUpMenuBotton extends StatefulWidget{
-    
+    //Esto tendria que ser traido de la base de datos ahora es una prueba
+    final List<String> categorias;
+
     CustomPopUpMenuBotton({
-        Key ? key,
-    }): super (key : key);
+        Key? key,
+        this.categorias = const ['Todos', 'Plastico', 'Papel y carton', 'Vidrio', 'Metales', 'Organico', 'Residuos electronicos', 'Textiles', 'Residuos peligrosos', 'Residuos voluminosos', 'Residuos de construccion y demolicion', 'Residuos sanitarios'],
+    }) : super(key: key);
 
     @override
     State<CustomPopUpMenuBotton> createState() => _CustomPopUpMenuBottonState();
@@ -15,9 +18,16 @@ class _CustomPopUpMenuBottonState extends State<CustomPopUpMenuBotton> with Sing
     @override
     Widget build(BuildContext context) {
         return PopupMenuButton<String>(
-            itemBuilder:(context) => [
-                
-            ],
+            offset: Offset(0, 50),
+            onSelected: (value) {
+                print("$value");//Deberia optimizar y hacer que sean los ids?
+            },
+            itemBuilder:(context) => List.generate(widget.categorias.length, 
+                (i) => PopupMenuItem(
+                    value: widget.categorias[i],
+                    child: Text(widget.categorias[i], style: Theme.of(context).textTheme.labelLarge,)
+                )
+            ),
         );
-    }
+    } 
 }
