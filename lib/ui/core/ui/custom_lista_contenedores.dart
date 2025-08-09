3d8606762157509a42488768f09e6751cdcf4446
Cristian <cristian.mg.david@gmail.com>
2025-08-09 18:51:48 -0300

    feat(custom_lista_contenedores): crear (GestureDetector)-m- Crear tamaño de lista-m- Crear textos indicativos-m- Crear icons

--- lib/ui/core/ui/custom_lista_contenedores.dart
+++ lib/ui/core/ui/custom_lista_contenedores.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class CustomListaContenedores extends StatefulWidget{
 
-  CustomListaContenedores({
-    Key ? key,
-  }): super (key: key);
+  CustomListaContenedores({ 
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<CustomListaContenedores> createState() => _CustomListaContenedoresState();
@@ -14,10 +14,42 @@ class _CustomListaContenedoresState extends State<CustomListaContenedores> with 
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        
-      ],
+    return GestureDetector(
+      onTap: () => print('Toco boton'),
+      child: Container(
+        height: 75,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(width: 1, color: Colors.grey[400]!),
+          borderRadius: BorderRadius.circular(5)
+        ),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: 50,
+                margin: EdgeInsets.symmetric(horizontal: 10),
+                child: Text('id contenedor', style: Theme.of(context).textTheme.labelMedium,)
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Nombre personalizado', style: Theme.of(context).textTheme.labelMedium),
+                  Text('tipo residuo', style: Theme.of(context).textTheme.labelMedium),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(onPressed: () {}, icon: Image.asset('assets/icons/mapa/pencil.png'),),
+                  IconButton(onPressed: () {}, icon: Image.asset('assets/icons/mapa/delete.png')),
+                ],
+              ),
+            ],
+        ),
+      ),
     );
   }
 }
