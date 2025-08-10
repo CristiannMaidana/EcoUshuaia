dff22ecc804ebe9e061cc97516744c1512b68aec
Cristian <cristian.mg.david@gmail.com>
2025-08-10 15:15:26 -0300

    feat(custom_mapa): crear propiedad al constructor y metodos-m- Crear propiedad (onMapReady) que es una funcion para el manejo del controlador del mapa-m- Crear metodo (_onMapCreated), para inicializar el mapa en la propiedad privada y controlador-m- Crear metodo dispose para desvincular el controlador-m- Reconstruir (MapWidget) en onMapCreated

--- lib/ui/core/ui/custom_mapa.dart
+++ lib/ui/core/ui/custom_mapa.dart
@@ -1,18 +1,23 @@
+import 'package:eco_ushuaia/ui/core/ui/custom_mapa_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-class CustomMapa extends StatefulWidget{
-  
-  CustomMapa({
+class CustomMapa extends StatefulWidget {
+  final void Function(CustomMapaController controller) onMapReady;
+
+  const CustomMapa({
     Key? key,
-  }): super (key: key);
+    required this.onMapReady,
+  }) : super(key: key);
 
   @override
   State<CustomMapa> createState() => _CustomMapaState();
 }
 
-class _CustomMapaState extends State<CustomMapa> with SingleTickerProviderStateMixin{
+class _CustomMapaState extends State<CustomMapa> {
   late final String accessToken;
+  MapboxMap? _mapboxMap;
+  final _controller = CustomMapaController(null);
 
   @override
   void initState() {
@@ -20,18 +25,24 @@ class _CustomMapaState extends State<CustomMapa> with SingleTickerProviderStateM
     accessToken = const String.fromEnvironment("ACCESS_TOKEN");
     MapboxOptions.setAccessToken(accessToken);
   }
+  
+  void _onMapCreated(MapboxMap map) {
+    _mapboxMap = map;
+    _controller.attach(map);
+    widget.onMapReady(_controller); 
+  }
 
-  CameraOptions camera = CameraOptions(
-    center: Point(coordinates: Position(-68.3030, -54.8019)),
-    zoom: 12,
-    bearing: 0,
-    pitch: 0,
-  );
+  @override
+  void dispose() {
+    _controller.detach();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MapWidget(
-      cameraOptions: camera,
+      key: const ValueKey("map"),
+      onMapCreated: _onMapCreated,
     );
   }
 }
