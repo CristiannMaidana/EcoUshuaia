ddcc6e0b43568aa2d6cd83985a6f16fccee8def0
Cristian <cristian.mg.david@gmail.com>
2025-08-11 12:36:41 -0300

    feat(custom_navegador_mapa): crear clase nueva-m- Crear estructura base de clase (CustomNavegadorMapa)

---
+++ lib/ui/core/ui/custom_navegador_mapa.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomNavegadorMapa extends StatefulWidget{
+  
+  CustomNavegadorMapa({
+    Key ? key,
+  }): super (key: key);
+
+  @override
+  State<CustomNavegadorMapa> createState() => _CustomNavegadorMapaState();
+}
+
+class _CustomNavegadorMapaState extends State<CustomNavegadorMapa> with SingleTickerProviderStateMixin{
+
+  @override
+  Widget build (BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
