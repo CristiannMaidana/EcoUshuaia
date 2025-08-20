d6e743584363c2e319afa4284c12f0a1a09dbe9b
Cristian <cristian.mg.david@gmail.com>
2025-08-20 17:29:34 -0300

    refactor(residuos_entity): cambiar nombre de archivo-m-Cambiar nombre de residuos a residuos_entity

--- lib/features/map/domain/entities/residuos.dart
+++
@@ -1,9 +0,0 @@
-class Residuos {
-  final String nombre;
-  final String categoria;
-  final double peso;
-  final String instruccion_reciclado;
-  final String descripcion;
-
-  Residuos({required this.nombre, required this.categoria, required this.peso, required this.instruccion_reciclado, required this.descripcion});
-}
---
+++ lib/features/map/domain/entities/residuos_entity.dart
@@ -0,0 +1,9 @@
+class ResiduosEntity {
+  final String nombre;
+  final String categoria;
+  final double peso;
+  final String instruccion_reciclado;
+  final String descripcion;
+
+  ResiduosEntity({required this.nombre, required this.categoria, required this.peso, required this.instruccion_reciclado, required this.descripcion});
+}
