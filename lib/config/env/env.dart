408ecd0fdc99d85eb2c4e1e0267ffcbe8e6db955
Cristian <cristian.mg.david@gmail.com>
2025-08-23 17:05:45 -0300

    feat(env): crear directorio y archivo env-m- Crear directorio lib/config/env/-m- Crear clase Env-m- Agregar el puerto BASE_URL-m- Crear tiempos de respuesta para testear la conexion

---
+++ lib/config/env/env.dart
@@ -0,0 +1,5 @@
+class Env {
+  static const String BASE_URL = 'http://127.0.0.1:8000/api/';
+  static const Duration connecTimeout = Duration(seconds: 15);
+  static const Duration receiveTimeout = Duration(seconds: 20);
+}
