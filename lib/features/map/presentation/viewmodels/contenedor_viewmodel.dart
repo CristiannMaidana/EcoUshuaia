6d569f9ba5a57f2149272edf9e0d10ebd125cd56
Cristian <cristian.mg.david@gmail.com>
2025-08-24 16:02:30 -0300

    feat(contendor_viewmodel): crear nuevo directorio y archivo-m- Crear nuevo directorio 'lib/features/map/presentation/viewmodels/'-m- Crear nueva clase 'ContenedorViewModel'-m- Crear notificaciones para saber el estado del objeto

---
+++ lib/features/map/presentation/viewmodels/contenedor_viewmodel.dart
@@ -0,0 +1,31 @@
+import 'package:eco_ushuaia/features/map/domain/entities/contenedor.dart';
+import 'package:eco_ushuaia/features/map/domain/repositories/contenedor_repository.dart';
+import 'package:flutter/widgets.dart';
+
+class ContenedorViewModel extends ChangeNotifier {
+  final ContenedorRepository repo;
+
+  ContenedorViewModel(this.repo);
+
+  bool _loading = false;
+  String? _error;
+  List<Contenedor> _items = const [];
+  
+  bool get loading => _loading;
+  String? get error => _error;
+  List<Contenedor> get items => _items;
+
+  Future<void> load() async {
+    _loading = true;
+    _error = null;
+    notifyListeners();
+    try {
+      _items = await repo.list();
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _loading = false;
+      notifyListeners();
+    }
+  }
+}
