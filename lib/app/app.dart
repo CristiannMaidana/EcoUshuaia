f0b260f934dbdc54014f05a29f5823a51cd7cc2c
Cristian <cristian.mg.david@gmail.com>
2025-08-26 18:34:26 -0300

    feat(app): envolver 'MaterialApp' en un provider-m- Agregar 'builAppProviders' para que todos los widgets hijos entiendan las notificaciones

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,16 +1,21 @@
+import 'package:eco_ushuaia/app/di/di.dart';
 import 'package:eco_ushuaia/features/shell/presentation/app_shell_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:eco_ushuaia/core/theme/app_theme.dart';
+import 'package:provider/provider.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'EcoUshuaia',
-      theme: appTheme,
-      home: ContainerHomeScreen(),
+    return MultiProvider(
+      providers: buildAppProviders(),
+      child: MaterialApp(
+        title: 'EcoUshuaia',
+        theme: appTheme,
+        home: ContainerHomeScreen(),
+      ),
     );
   }
 }
