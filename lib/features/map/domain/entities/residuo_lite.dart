0bf68b75aba1569e69caf489099d9f16d1c20d83
Cristian <cristian.mg.david@gmail.com>
2025-08-26 18:55:02 -0300

    feat(residuo_lite): crear nueva entidad-m- Crear entidad para la tabla relacional (residuos) de 'contenedores'

---
+++ lib/features/map/domain/entities/residuo_lite.dart
@@ -0,0 +1,11 @@
+class ResiduoLite {
+  final int id;
+  final String? nombre;
+  final String? categoria;
+
+  const ResiduoLite({
+    required this.id,
+    required this.nombre,
+    required this.categoria, 
+  });
+}
