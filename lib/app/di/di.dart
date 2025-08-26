0497b4c10e4671f125bea840b43439915dfa9225
Cristian <cristian.mg.david@gmail.com>
2025-08-26 18:32:21 -0300

    feat(di): refactorizar y agregar contenedores-m- Separar en metodos privados los providers-m- Crear los providers para el manejo de 'contenedores'

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -1,3 +1,6 @@
+import 'package:eco_ushuaia/features/map/data/repositories/contenedor_repository_imp.dart';
+import 'package:eco_ushuaia/features/map/data/sources/remote/contenedor_remote_data_source.dart';
+import 'package:eco_ushuaia/features/map/domain/repositories/contenedor_repository.dart';
 import 'package:eco_ushuaia/features/waste/data/repositories/residuo_repository_imp.dart';
 import 'package:eco_ushuaia/features/waste/domain/repositories/residuo_repository.dart';
 import 'package:http/http.dart' as http;
@@ -7,7 +10,7 @@ import 'package:provider/single_child_widget.dart';
 import '../../core/network/http_client.dart';
 import '../../features/waste/data/sources/residuos_remote_data_source.dart';
 
-List<SingleChildWidget> buildAppProviders() => [
+List<SingleChildWidget> _coreProviders() => [
   Provider<http.Client>(
     create: (_) => http.Client(),
     dispose: (_, client) => client.close(),
@@ -15,10 +18,28 @@ List<SingleChildWidget> buildAppProviders() => [
   ProxyProvider<http.Client, ApiClient>(
     update: (_, client, __) => ApiClient(client),
   ),
+];
+
+List<SingleChildWidget> _residuosProviders() => [
   ProxyProvider<ApiClient, ResiduosRemoteDataSource>(
     update: (_, api, __) => ResiduosRemoteDataSource(api),
   ),
   ProxyProvider<ResiduosRemoteDataSource, ResiduosRepository>(
     update: (_, ds, __) => ResiduosRepositoryImpl(ds),
   ),
+];
+
+List<SingleChildWidget> _contenedoresProviders() => [
+  ProxyProvider<ApiClient, ContenedorRemoteDataSource>(
+    update: (_, api, __) => ContenedorRemoteDataSource(api),
+  ),
+  ProxyProvider<ContenedorRemoteDataSource, ContenedorRepository>(
+    update: (_, ds, __) => ContenedorRepositoryImp(ds),
+  ),
+];
+
+List<SingleChildWidget> buildAppProviders() => [
+  ..._coreProviders(),
+  ..._residuosProviders(),
+  ..._contenedoresProviders(),
 ];
