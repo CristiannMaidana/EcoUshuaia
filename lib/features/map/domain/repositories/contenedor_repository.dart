e682d8b1eae9b9bb7041d863dd7aa7dfd34c6207
Cristian <cristian.mg.david@gmail.com>
2025-08-26 19:11:17 -0300

    feat(contenedor_repository): agregar propiedad en metodo-m- En metodo abstractor agregar propiedad 'filtros'

--- lib/features/map/domain/repositories/contenedor_repository.dart
+++ lib/features/map/domain/repositories/contenedor_repository.dart
@@ -1,5 +1,5 @@
 import 'package:eco_ushuaia/features/map/domain/entities/contenedor.dart';
 
 abstract class ContenedorRepository {
-  Future<List<Contenedor>> list();
+  Future<List<Contenedor>> list({Map<String, dynamic>? filtros});
 }
