6d9fb43ba60f815c82b30a38dd105e508202b03f
Cristian <cristian.mg.david@gmail.com>
2025-08-27 16:53:20 -0300

    feat(textFormFieldDataUser): crear nuevo widget-m- Crear widget 'TextFormFieldDataUser'-m- Crear propiedades constructor y diseño del input

---
+++ lib/features/auth/presentation/widgets/textFormFieldDataUser.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+typedef StringValidator = String? Function(String?);
+
+class TextFormFieldDataUser extends StatefulWidget{
+  final Widget? lottie;
+  final StringValidator? validate;
+  final String nombre;
+  final FocusNode? focusNode;
+
+  TextFormFieldDataUser({
+    Key ? key,
+    required this.lottie,
+    required this.validate,
+    required this.nombre,
+    required this.focusNode,
+  }): super(key: key);
+
+  @override
+  State<TextFormFieldDataUser> createState() => _TextFormFieldDataUserState();
+}
+
+class _TextFormFieldDataUserState extends State<TextFormFieldDataUser> {
+  @override
+  Widget build(BuildContext context){
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(widget.nombre, style: Theme.of(context).textTheme.bodyLarge),
+          TextFormField(
+            focusNode: widget.focusNode,
+            decoration: InputDecoration(
+              labelText: widget.nombre,
+              contentPadding: EdgeInsets.all(13),
+              labelStyle: Theme.of(context).textTheme.labelLarge,
+              errorStyle: Theme.of(context).textTheme.labelSmall,
+              prefixIcon: widget.lottie == null ? null : Padding(
+                padding: EdgeInsets.only(left: 12),
+                child: widget.lottie,
+              ),
+            ),
+            validator: widget.validate,
+          ),
+        ],
+      ),
+    );
+  }
+}
