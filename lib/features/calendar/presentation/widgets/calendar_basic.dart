5d147b43173d86f0063e12248cf676f781dad14a
Cristian <cristian.mg.david@gmail.com>
2025-10-16 17:52:12 -0300

    feat(calendar_basic): reestructuracion de clase-m- Crear clase 'StatefulWidget'-m- Crear variable de valores iniciales, 'format' para los dias visibles del calendario. '_focusDay' para saber la fecha actual. '_selectedDay' para visualizar el dia que selecciono-m- Contruir el widget calendar, delimitar con 'firstDay' y 'lastDay' la fecha de visualizacion del calendario, habilitar todos los gestos para el calendario. Agregar los gestos y manejo de eventos

--- lib/features/calendar/presentation/widgets/calendar_basic.dart
+++ lib/features/calendar/presentation/widgets/calendar_basic.dart
@@ -1,7 +1,53 @@
+import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-final TableCalendar calendario = TableCalendar(
-  firstDay: DateTime.utc(2010, 1, 1),
-  lastDay: DateTime.utc(2030, 1, 1),
-  focusedDay: DateTime.now(),
-);
+class CalendarioWidget extends StatefulWidget {
+  const CalendarioWidget({super.key});
+
+  @override
+  State<CalendarioWidget> createState() => _CalendarioWidgetState();
+}
+
+class _CalendarioWidgetState extends State<CalendarioWidget> {
+  CalendarFormat _format = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      firstDay: DateTime.utc(2024, 1, 1),
+      lastDay: DateTime.utc(2026, 1, 1),
+      focusedDay: _focusedDay,
+
+      calendarFormat: _format,
+      availableGestures: AvailableGestures.all,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+
+      // Selección de día
+      selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _selectedDay = selectedDay;
+          _focusedDay = focusedDay; 
+        });
+      },
+
+      onPageChanged: (focusedDay) {
+        _focusedDay = focusedDay;
+      },
+
+      // Toggle de formato (Mes / 2 semanas / Semana)
+      onFormatChanged: (format) {
+        if (_format != format) {
+          setState(() => _format = format);
+        }
+      },
+
+      headerStyle: const HeaderStyle(
+        titleCentered: true,
+        formatButtonVisible: true,
+      ),
+    );
+  }
+}
