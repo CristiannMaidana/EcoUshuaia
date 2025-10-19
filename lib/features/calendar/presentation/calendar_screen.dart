706c0f95291822fe47c434ab9fef2e9324047e85
Cristian <cristian.mg.david@gmail.com>
2025-10-19 17:45:51 -0300

    feat(calendar_screen): agregar paquete-m- Agregar paquete 'intl'

--- lib/features/calendar/presentation/calendar_screen.dart
+++ lib/features/calendar/presentation/calendar_screen.dart
@@ -3,6 +3,7 @@ import 'package:eco_ushuaia/core/ui/buttons/standard_button.dart';
 import 'package:eco_ushuaia/features/calendar/presentation/widgets/calendar_basic.dart';
 import 'package:eco_ushuaia/features/news/presentation/widgets/novedades_sheet.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class CalenderScreen extends StatefulWidget {
   const CalenderScreen({super.key});
