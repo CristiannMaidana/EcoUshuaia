2b50cdf69fddf0ddbd1ee6766485600d7bd390c3
Cristian <cristian.mg.david@gmail.com>
2025-10-19 20:59:11 -0300

    feat(calendarios): crear dominio-m- Crear nuevo directorio 'entities'-m- Crear nueva clase con constructor 'Calendarios'

---
+++ lib/features/calendar/domain/entities/calendarios.dart
@@ -0,0 +1,11 @@
+class Calendarios {
+  final int idCalendario;
+  final String novedad;
+  final DateTime fechaHora;
+
+  const Calendarios({
+    required this.idCalendario,
+    required this.novedad,
+    required this.fechaHora,
+  });  
+}
