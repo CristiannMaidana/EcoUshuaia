e36708497e3542f5316bbc375c98bc3e318bdee7
Cristian <cristian.mg.david@gmail.com>
2025-10-20 20:29:59 -0300

    feat(calendarios): crear nueva propiead-m- Crear nueva propiedad 'titulo' de tipo String-m- Agregar 'titulo' en constructor

--- lib/features/calendar/domain/entities/calendarios.dart
+++ lib/features/calendar/domain/entities/calendarios.dart
@@ -2,10 +2,12 @@ class Calendarios {
   final int idCalendario;
   final String novedad;
   final DateTime fechaHora;
+  final String titulo;
 
   const Calendarios({
     required this.idCalendario,
     required this.novedad,
     required this.fechaHora,
+    required this.titulo,
   });  
 }
