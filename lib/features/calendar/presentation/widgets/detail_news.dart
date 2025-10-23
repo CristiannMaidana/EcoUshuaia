b5a584043eef12fb6bec0989ffe809597763ca92
Cristian <cristian.mg.david@gmail.com>
2025-10-23 20:46:58 -0300

    feat(detail_news): agregar propiedad y crear texto-m- Crear propiedad 'onClose' de tipo VoidCallBack y agregarla al constructor-m- Crear botones de cerrar, agregar recordatorio y ver en el mapa-m- Crear las areas de texto y botones del widget

--- lib/features/calendar/presentation/widgets/detail_news.dart
+++ lib/features/calendar/presentation/widgets/detail_news.dart
@@ -1,11 +1,13 @@
-import 'package:eco_ushuaia/core/ui/layout/spacing.dart';
 import 'package:eco_ushuaia/features/calendar/domain/entities/calendarios.dart';
+import 'package:eco_ushuaia/features/news/presentation/widgets/info_grind.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class DetailNews extends StatelessWidget{
   final Calendarios? newCalendar;
+  final VoidCallback onClose;
 
-  const DetailNews({super.key, this.newCalendar});
+  const DetailNews({super.key, this.newCalendar, required this.onClose});
 
   @override
   Widget build(BuildContext context) {
@@ -14,21 +16,133 @@ class DetailNews extends StatelessWidget{
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(c.titulo, style: Theme.of(context).textTheme.headlineSmall),
-        espacioVerticalMediano,
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Icon(Icons.calendar_month, size: 20, color: Colors.grey),
-            const SizedBox(width: 8),
-            Text('Fecha', style: Theme.of(context).textTheme.labelMedium),
-            const SizedBox(width: 8),
-            // si tienes c.fechaHora:
-            // Text(DateFormat('dd/MM/yy – HH:mm').format(c.fechaHora)),
-          ],
-        ),espacioVerticalMediano,
-        Text(c.novedad, style: Theme.of(context).textTheme.bodyMedium),
+        //Header
+        Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Botones
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  //Mapa
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(32),
+                      border: Border.all(color: Colors.grey[400]!, width: 1),
+                    ),
+                    child: IconButton(onPressed: onClose, icon: Icon(Icons.map))
+                  ),
+                  SizedBox(width: 10,),
+                  //Recordatorio
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(32),
+                      border: Border.all(color: Colors.grey[400]!, width: 1),
+                    ),
+                    child: IconButton(onPressed: onClose, icon: Icon(Icons.add))
+                  ),
+                  SizedBox(width: 10,),
+                  //Cerrar
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(32),
+                      border: Border.all(color: Colors.grey[400]!, width: 1),
+                    ),
+                    child: IconButton(onPressed: onClose, icon: Icon(Icons.close))
+                  ),
+                ],
+              ),
+              //Texto Categorias
+              Container(
+                width: 150,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.grey[200],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.message_sharp),
+                    SizedBox(width: 10),
+                    Text("Categoria", style: Theme.of(context).textTheme.labelMedium),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10,),
+              //Titulo Noticia
+              Text(c.titulo, style: Theme.of(context).textTheme.headlineSmall),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+        //Datos fecha
+        _lineDivider(context),
+        Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(DateFormat('dd/MM/yy – HH:mm').format(c.fechaHora), style: Theme.of(context).textTheme.labelMedium,),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey, width: 1),
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  child: TextButton(
+                    onPressed: () =>{},
+                    child: Row(
+                      children: [
+                        Icon(Icons.arrow_drop_down_sharp, color: Colors.black, size: 30,),
+                        Text("Detalle de fecha", style: Theme.of(context).textTheme.labelMedium,),
+                      ],
+                    )
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        //Datos residuo o nada
+        _lineDivider(context),
+        Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: InfoGrid(
+              items: const [
+                InfoItem('Zona', 'Centro'),
+                InfoItem('Categoría', 'Orgánicos'),
+                InfoItem('Responsable', 'Higiene Urbana'),
+                InfoItem('Contacto', '0800-XXX-1234'),
+              ],
+            ),
+          ),
+        ),
+        //Texto de novedead
+        _lineDivider(context),
+        Container(
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsetsGeometry.symmetric(horizontal: 20, vertical: 10),
+            child: Text(c.novedad, style: Theme.of(context).textTheme.bodyMedium),
+          ),
+        ),
       ],
     );
   }
+}
+
+Divider _lineDivider (BuildContext context){
+  return Divider(
+    height: 1,
+    color: Colors.black,
+  );
 }
