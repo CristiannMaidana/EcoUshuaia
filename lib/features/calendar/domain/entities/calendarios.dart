bd1b97a82355863a254b54eea059c7e1ff5bfa6a
Cristian <cristian.mg.david@gmail.com>
2025-10-24 18:27:12 -0300

    feat(calendarios): actualizar entitie-m- Agregar a entitie 'Calendarios' propiedades duracion, subtitulo, todoElDia, caregoriaNoticia, creadoAt y dividir fechaHora en fecha y hora

--- lib/features/calendar/domain/entities/calendarios.dart
+++ lib/features/calendar/domain/entities/calendarios.dart
@@ -1,13 +1,25 @@
 class Calendarios {
   final int idCalendario;
-  final String novedad;
-  final DateTime fechaHora;
+  final String cuerpo;
+  final DateTime fecha;
+  final Duration hora;
   final String titulo;
+  final Duration duracion;
+  final String? subtitulo;
+  final bool todoElDia;
+  final String categoriaNoticia;
+  final DateTime creadoAt;
 
   const Calendarios({
     required this.idCalendario,
-    required this.novedad,
-    required this.fechaHora,
+    required this.cuerpo,
+    required this.fecha,
+    required this.hora,
     required this.titulo,
+    required this.duracion,
+    required this.subtitulo,
+    required this.todoElDia,
+    required this.categoriaNoticia,
+    required this.creadoAt,
   });  
 }
