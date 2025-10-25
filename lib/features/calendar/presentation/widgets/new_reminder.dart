c9f1862c9c02340440da4cbdf204b08f08a70539
Cristian <cristian.mg.david@gmail.com>
2025-10-25 20:28:04 -0300

    feat(new_reminder): crear area de titulo y notas-m- Crear widget re utilizable '_label' para el ingreso de texto de titulo y notas-m- Crear '_scrollCrtlTitulo' y '_scrollCrtNotas' para el manejo de desplazamiento de 'TextFormField'

--- lib/features/calendar/presentation/widgets/new_reminder.dart
+++ lib/features/calendar/presentation/widgets/new_reminder.dart
@@ -10,6 +10,16 @@ class NewReminder extends StatefulWidget{
 }
 
 class _StateNewReminder extends State<NewReminder>{
+  final _scrollCtrlTitulo = ScrollController();
+  final _scrollCtrlNotas = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollCtrlTitulo.dispose();
+    _scrollCtrlNotas.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context){
     return Container(
@@ -32,9 +42,45 @@ class _StateNewReminder extends State<NewReminder>{
                 CircleIcon(icon: Icons.check_sharp, onPressed: () {}),
               ],
             ),
-          ],
+            //Titulo y nota
+            SizedBox(height: 20,),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.black38),
+                borderRadius: BorderRadius.circular(32)
+              ),
+              child: Column(
+                children: [
+                  _label(context, _scrollCtrlTitulo, "Título"),
+                  _label(context, _scrollCtrlNotas, "Notas"),
+                ],
+              ),
+            )
+            ],
         ),
       ),
     );
   }
+}
+
+Widget _label(context, _scrollCtrl, label){
+  return Scrollbar(
+    controller: _scrollCtrl,
+    thumbVisibility: true,
+    child: TextFormField(
+      keyboardType: TextInputType.multiline,
+      minLines: 1,
+      maxLines: 4,
+      scrollController: _scrollCtrl,
+      style: Theme.of(context).textTheme.labelLarge,
+      decoration: InputDecoration(
+        hintText: label,
+        border: InputBorder.none,
+        enabledBorder: InputBorder.none,
+        focusedBorder: InputBorder.none,
+        filled: true,
+        fillColor: Colors.transparent,
+      ),  
+    ),
+  ); 
 }
