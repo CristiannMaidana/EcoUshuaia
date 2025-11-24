3577d76ca660b108c371df270d952921c38e8b1a
Cristian <cristian.mg.david@gmail.com>
2025-11-24 18:44:48 -0300

    feat(categoria_notcias): crear nueva entitie-m- Crear entitie 'CategoriaNoticias'-m- Agregar propiedades 'idCategoriaNoticias', 'categoria' y 'colorHex'

---
+++ lib/features/calendar/domain/entities/categoria_noticias.dart
@@ -0,0 +1,11 @@
+class CategoriaNoticias {
+  final int idCategoriaNoticias;
+  final String categoria;
+  final String colorHex;
+
+  const CategoriaNoticias({
+    required this.idCategoriaNoticias,
+    required this.categoria,
+    required this.colorHex,
+  });
+}
