ae1165b7509b880eaff19d5d6f1d669644c4cd31
Cristian <cristian.mg.david@gmail.com>
2025-12-12 00:36:22 -0300

    feat(info_state_container): crear nuevo widget-m- Crear widget 'InfoStateContainer', crear propiedades titulo, descripcion de tipo String-m- Crear buil 'Container'

---
+++ lib/features/map/presentation/widgets/info_state_container.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class InfoStateContainer extends StatelessWidget{
+  final String titulo;
+  final String descripcion;
+
+  const InfoStateContainer({
+    super.key, 
+    required this.titulo,
+    required this.descripcion,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
