95bcff670c45aa00a0584eeba78492799669dfb5
Cristian <cristian.mg.david@gmail.com>
2025-12-12 00:27:31 -0300

    feat(data_container): crear nuevo widget-m- Crear widget 'DataContainer', con propiedades 'contenido', 'icon' y 'colorIcon'-m- Crear buil 'Container'

---
+++ lib/features/map/presentation/widgets/data_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DataContainer extends StatelessWidget{
+  final String contenido;
+  final IconData icon;
+  final Color colorIcon;
+
+  const DataContainer({
+    super.key, 
+    required this.contenido,
+    required this.icon,
+    required this.colorIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
