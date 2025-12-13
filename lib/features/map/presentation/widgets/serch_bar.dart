4681db1447b64438933c53f74348c49b2be0b66d
Cristian <cristian.mg.david@gmail.com>
2025-12-12 22:32:04 -0300

    feat(serch_bar): agregar widgets a container-m- En Row de Container agregar Icon y text widgets

--- lib/features/map/presentation/widgets/serch_bar.dart
+++ lib/features/map/presentation/widgets/serch_bar.dart
@@ -24,12 +24,24 @@ class _SerchBarState extends State<SerchBar> with SingleTickerProviderStateMixin
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //Barra de busqueda
             Expanded(
               child: Container(
+                height: 50,
                 margin: EdgeInsets.all(10),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(28),
                   border: Border.all(width: 1, color: Colors.black54),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(width: 10),
+                    Icon(Icons.search, color: Colors.black54),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Text('Ingrese una dirección', style: Theme.of(context).textTheme.labelLarge),
+                    ),
+                  ],
                 ),
               ),
             )
