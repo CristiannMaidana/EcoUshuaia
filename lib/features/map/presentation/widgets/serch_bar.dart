b0c522c9761a11b8c9523726a93604a66a46181f
Cristian <cristian.mg.david@gmail.com>
2025-12-12 22:07:37 -0300

    feat(serch_bar): crear nuevo widget-m- Crear widget 'SerchBar'-m- Crear widget 'GestureDetector'

---
+++ lib/features/map/presentation/widgets/serch_bar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SerchBar extends StatefulWidget{
+  SerchBar({Key ? key}): super (key: key);
+
+  @override
+  State<SerchBar> createState() => _SerchBarState();
+}
+
+class _SerchBarState extends State<SerchBar> with SingleTickerProviderStateMixin{
+
+  @override
+  Widget build (BuildContext context) {
+    return GestureDetector();
+  }
+}
