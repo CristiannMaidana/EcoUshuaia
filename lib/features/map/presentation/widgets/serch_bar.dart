2dcfeb438fb1a2a3ac3d17654e7ce40b2397e577
Cristian <cristian.mg.david@gmail.com>
2025-12-12 22:13:47 -0300

    feat(serch_bar): agregar hijo a build-m- Crear hijo 'Container' en 'GestureDetector'-m- Agregar ancho y alto y decoration

--- lib/features/map/presentation/widgets/serch_bar.dart
+++ lib/features/map/presentation/widgets/serch_bar.dart
@@ -11,6 +11,17 @@ class _SerchBarState extends State<SerchBar> with SingleTickerProviderStateMixin
 
   @override
   Widget build (BuildContext context) {
-    return GestureDetector();
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 60,
+        width: 400,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(28),
+          border: Border.all(width: 1, color: Colors.black54),
+        ),
+      ),
+    );
   }
 }
