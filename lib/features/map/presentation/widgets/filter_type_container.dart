6f826da0f4dda5ae4aee3232c8f468ff2d61efa3
Cristian <cristian.mg.david@gmail.com>
2025-12-13 15:50:03 -0300

    feat(filter_type_container): cambiar estado de widget-m- Convertir widget 'FilterTypeContainer' en un stateful-m- Crear clase de estado del widget

--- lib/features/map/presentation/widgets/filter_type_container.dart
+++ lib/features/map/presentation/widgets/filter_type_container.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class FilterTypeContainer extends StatelessWidget {
+class FilterTypeContainer extends StatefulWidget {
   final List<String> categorias;
   final ValueChanged<String> onSelected;
 
@@ -11,6 +11,11 @@ class FilterTypeContainer extends StatelessWidget {
   });
 
   @override
+  State<FilterTypeContainer> createState() => _FilterPopoverAnchorState();
+}
+
+class _FilterPopoverAnchorState extends State<FilterTypeContainer>{
+  
   Widget build(BuildContext context) {
     return Stack();
   }
