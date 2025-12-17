b7f1691a8072088144398b78d60a10e654448b86
Cristian <cristian.mg.david@gmail.com>
2025-12-17 19:10:08 -0300

    feat(content_filter): crear nuevo widget-m- Crear widget 'ContentFilter'-m- Crear estado del widget '_ContentFilterState'-m- Crear buil 'Container'

---
+++ lib/features/map/presentation/widgets/content_filter.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ContentFilter extends StatefulWidget {
+  const ContentFilter({super.key});
+
+  @override
+  State<ContentFilter> createState() => _ContentFilterState();
+}
+
+class _ContentFilterState extends State<ContentFilter> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
