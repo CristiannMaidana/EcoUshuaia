606814ee1613866e6a59c6e248a6fbe0e078ce02
Cristian <cristian.mg.david@gmail.com>
2025-12-17 14:38:28 -0300

    feat(header_filter): crear nuevo widget-m- Crear widget HeaderFilter, sin estado del widget-m- En build de widget crear tipo Column

---
+++ lib/features/map/presentation/widgets/header_filter.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HeaderFilter extends StatelessWidget{
+  
+  const HeaderFilter ({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
