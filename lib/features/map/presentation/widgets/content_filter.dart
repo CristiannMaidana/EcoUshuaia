facb9fcfd8db784ebc80fd7932f4e054830d73fe
Cristian <cristian.mg.david@gmail.com>
2025-12-17 19:21:11 -0300

    feat(content_filter): agregar seccion de filtro-m- Agregar 'Padding' con hijo 'ExpansionTileCustom' con titulo, hijo y estado del boton, para la seccion de estado del contenedor-m- Importar librerias 'custom_button_filter'-m- Crear variable para pobrar el boton 'filtroActivo'

--- lib/features/map/presentation/widgets/content_filter.dart
+++ lib/features/map/presentation/widgets/content_filter.dart
@@ -1,4 +1,5 @@
 import 'package:eco_ushuaia/core/theme/colors.dart';
+import 'package:eco_ushuaia/features/calendar/presentation/widgets/custom_button_filter.dart';
 import 'package:eco_ushuaia/features/map/presentation/widgets/expansion_tile_custom.dart';
 import 'package:eco_ushuaia/features/map/presentation/widgets/filter_container.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +12,8 @@ class ContentFilter extends StatefulWidget {
 }
 
 class _ContentFilterState extends State<ContentFilter> {
+  // TODO: cambiar variable para que no sobreescriba en todos los casos o borrar?
+  bool filtroActivo = false;
   
   @override
   Widget build(BuildContext context) {
@@ -32,9 +35,36 @@ class _ContentFilterState extends State<ContentFilter> {
                 ],
               ),
             ),
-          ),            
-          // TODO: padding con hijo ExpansionTileCustom para la seleccion de estado del contenedor
+          ),
 
+          // Seccion estado del contenedor
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10, top: 6),
+            child: ExpansionTileCustom(
+              title: 'Estado',
+              initiallyOpen: true,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: CustomButtonFilter(label: 'Operativo', selected: filtroActivo, onTap: () {
+                      setState(() {
+                        filtroActivo = !filtroActivo;
+                        print(filtroActivo);
+                      });
+                    },),
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: CustomButtonFilter(label: 'En mantenimiento', selected: filtroActivo, onTap: () {
+                      setState(() {
+                        filtroActivo = !filtroActivo;
+                      });
+                    },),
+                  ),
+                ],
+              )
+            ),
+          ),
           // TODO: padding con hijo ExpansionTileCustom para la seleccion de dias de recoleccion
 
           // TODO: padding con hijo ExpansionTileCustom para la seleccion de nivel de llenado del contenedor
