25dd2f61312f431f4641b17d55ac1da1b16b9abc
Cristian <cristian.mg.david@gmail.com>
2025-12-17 14:45:33 -0300

    feat(header_filter): agregar widget a Row-m- Agregar widget 'SizedBox', para representar el boton secundario de limpiar filtros-m- Agregar ancho y alto, y texto

--- lib/features/map/presentation/widgets/header_filter.dart
+++ lib/features/map/presentation/widgets/header_filter.dart
@@ -19,6 +19,21 @@ class HeaderFilter extends StatelessWidget{
                 //Titulo
                 Text('Filtros', style: Theme.of(context).textTheme.titleLarge),
                 const Spacer(),
+                
+                //Boton limpiar filtros
+                SizedBox(
+                  height: 36, 
+                  width: 93,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      side: BorderSide(color: Colors.grey)
+                    ),
+                    onPressed: () {}, // TODO: agregar metodo reseteador de filtros 
+                    child: const Text('Limpiar', style: TextStyle(fontSize: 13))
+                  ),
+                ),
+                const SizedBox(width: 8),
               ],
             ),
           ),
