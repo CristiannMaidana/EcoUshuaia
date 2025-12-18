8801123f96a12b343abfbd92f09ea730a939301b
Cristian <cristian.mg.david@gmail.com>
2025-12-18 11:54:11 -0300

    feat(custom_button_filter): borrar metodo-m- Borrar metodo 'initState'

--- lib/features/map/presentation/widgets/custom_button_filter.dart
+++ lib/features/map/presentation/widgets/custom_button_filter.dart
@@ -17,17 +17,13 @@ class CustomButtonFilter extends StatefulWidget{
 class _CustomButtonFilterState extends State<CustomButtonFilter> {
   late bool _selected;
 
-  @override
-  void initState() {
-    super.initState();
-    _selected = false;
-  }
-
   void _setSelected() {
     setState(() {
       _selected = !_selected;
     });
   }
+
+  // TODO: crear metodo para reiniciar estado del boton 
 
   @override
   Widget build (BuildContext context){
