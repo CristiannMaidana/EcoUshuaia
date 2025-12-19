2ecb83b0a126ff7a6231e6e0c768f7b4b6d236ef
Cristian <cristian.mg.david@gmail.com>
2025-12-19 12:00:48 -0300

    feat(button_filter_viewmodel): crear nuevo metodo
    - Crear metodo 'togller' para guardar el boton seleccionado o eliminar el boton seleccionado y actualizar cambio visual en el boton

--- lib/features/map/presentation/viewmodels/button_filter_viewmodel.dart
+++ lib/features/map/presentation/viewmodels/button_filter_viewmodel.dart
@@ -9,7 +9,16 @@ class ButtonFilterViewmodel extends ChangeNotifier{
   // Obtener el valor boolean para cambiar el color del estado del boton
   bool isSelected(String boton) => _selected.contains(boton);
 
-  // TODO: crear metodo para agrega o quita el boton seleccionado para actualizar el estado
+  // Metodo para agrega o quita el boton seleccionado y actualizar el estado
+  void toggle(String boton){
+    if (_selected.contains(boton)){
+      _selected.remove(boton);
+    }
+    else {
+      _selected.add(boton);
+    }
+    notifyListeners();
+  }
 
   // TODO: crear metodo para limpiar todos los filtros
 }
