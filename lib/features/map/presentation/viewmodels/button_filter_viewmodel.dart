d309b5dd4166f58a55a8829593b882a9eb3ca8aa
Cristian <cristian.mg.david@gmail.com>
2025-12-19 11:56:08 -0300

    feat(button_filter_viewmodel): crear nuevo viewmodel
    - Crear viewmodel 'ButtonFilterWidget', para el manejo de estado de los botones de filtro
    
    - Crear variable '_selected', para obtener los botones

---
+++ lib/features/map/presentation/viewmodels/button_filter_viewmodel.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ButtonFilterViewmodel extends ChangeNotifier{
+  final Set<String> _selected = {};
+
+  // TODO: crear metodo para obtener el elemento
+
+  // TODO: crear metodo par obtener el valor boolean para cambiar el color del estado del boton
+
+  // TODO: crear metodo para agrega o quita el boton seleccionado para actualizar el estado
+
+  // TODO: crear metodo para limpiar todos los filtros
+}
