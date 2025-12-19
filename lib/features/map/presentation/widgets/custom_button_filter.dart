27e01d9e3ea67973a928eb849805bac359dd2815
Cristian <cristian.mg.david@gmail.com>
2025-12-19 15:15:05 -0300

    feat(custom_button_filter): reestructurar widget
    - Cambiar estado de widget a 'StatelessWidget'
    
    - Cambiar propiedad 'onTap' a opcional
    
    - Cambiar widget a Selector de tipo map de 'ButtonFilterViewmodel y bool'
    
    - Crear boton con misma personalizacion
    
    - En funcion del boton agregar vm del provider y activar metodo 'toggle' del provider
    
    - Importar librerias 'provider' y 'button_filter_viewmodel'

--- lib/features/map/presentation/widgets/custom_button_filter.dart
+++ lib/features/map/presentation/widgets/custom_button_filter.dart
@@ -1,47 +1,40 @@
+import 'package:eco_ushuaia/features/map/presentation/viewmodels/button_filter_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class CustomButtonFilter extends StatefulWidget{
+class CustomButtonFilter extends StatelessWidget {
   final String label;
-  final VoidCallback onTap;
+  final VoidCallback? onTap;
 
   const CustomButtonFilter({
     super.key,
     required this.label,
-    required this.onTap,
+    this.onTap,
   });
 
   @override
-  State<CustomButtonFilter> createState() => CustomButtonFilterState();
-}
+  Widget build(BuildContext context) {
+    return Selector<ButtonFilterViewmodel, bool>(
+      selector: (_, vm) => vm.isSelected(label),
+      builder: (context, selected, _) {
+        final bgColor = selected ? const Color.fromARGB(255, 214, 255, 219) : Colors.white;
+        final brColor = selected ? const Color.fromARGB(255, 56, 67, 57) : Colors.grey;
 
-class CustomButtonFilterState extends State<CustomButtonFilter> {
-  late bool _selected = false;
-
-  void _setSelected() {
-    setState(() {
-      _selected = !_selected;
-    });
-  }
-
-  void resetBottom() {
-    setState(() {
-      _selected = false;
-    });
-  }
-
-  @override
-  Widget build (BuildContext context){
-    final bgColor = _selected ? const Color.fromARGB(255, 214, 255, 219) : Colors.white;
-    final bgborde = _selected ? const Color.fromARGB(255, 56, 67, 57) : Colors.grey;
-
-    return OutlinedButton(
-      style: OutlinedButton.styleFrom(
-        backgroundColor: bgColor,
-        foregroundColor: Colors.black,
-        side: BorderSide(width: 1, color: bgborde),
-      ),
-      onPressed: _setSelected, 
-      child: Text(widget.label, style: TextStyle(fontSize: 13),)
+        return OutlinedButton(
+          style: OutlinedButton.styleFrom(
+            backgroundColor: bgColor,
+            foregroundColor: Colors.black,
+            side: BorderSide(width: 1, color: brColor),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          ),
+          onPressed: () {
+            final vm = context.read<ButtonFilterViewmodel>();
+            vm.toggle(label);
+            // TODO: metodo asincronico para actualizar filtros en mapa
+          },
+          child: Text(label, style: const TextStyle(fontSize: 13)),
+        );
+      },
     );
   }
 }
