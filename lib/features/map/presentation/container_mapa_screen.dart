f0a463902ee3849e70274b2a72370c1daf1da2e2
Cristian <cristian.mg.david@gmail.com>
2025-12-19 18:45:32 -0300

    refactor(container_mapa_screen): reestructuracion de widget
    - Crear constructor
    
    - Cambiar widget de 'DefaultTabController' a 'Scaffold', eliminando el tab superior para navegar entre paginas y dejando unicamente el widget 'mapa_screen'
    
    - Eliminar importaciones

--- lib/features/map/presentation/container_mapa_screen.dart
+++ lib/features/map/presentation/container_mapa_screen.dart
@@ -1,9 +1,8 @@
-import 'package:eco_ushuaia/core/theme/colors.dart';
-import 'package:eco_ushuaia/features/map/presentation/screens/contenedores_screen.dart';
 import 'package:eco_ushuaia/features/map/presentation/screens/mapa_screen.dart';
 import 'package:flutter/material.dart';
 
 class ContainerMapaScreen extends StatefulWidget{
+  const ContainerMapaScreen({super.key});
 
   @override
   State<ContainerMapaScreen> createState() => _MapaScreenState();
@@ -12,30 +11,8 @@ class ContainerMapaScreen extends StatefulWidget{
 class _MapaScreenState extends State<ContainerMapaScreen> with SingleTickerProviderStateMixin {
   @override
   Widget build(context){
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: camarone400,
-          toolbarHeight: 0,
-          bottom: TabBar(
-            indicatorSize: TabBarIndicatorSize.tab,
-            unselectedLabelColor: Colors.grey[600],
-            isScrollable: false,
-            labelStyle: Theme.of(context).textTheme.labelLarge,
-            tabs: const [
-              Tab(text: 'Contenedores'),
-              Tab(text: 'Mapa'),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: [
-            ContenedoresScreen(),
-            MapaScreen(),
-          ],
-        ),
-      ),
+    return Scaffold(
+        body: MapaScreen(),
     );
   }
 }
