969e667eee74e3d0a0a38372a7c67e17d2b5f385
Cristian <cristian.mg.david@gmail.com>
2025-12-22 13:05:03 -0300

    feat(categoria_residuos_dto): crear metodo
    - Crear metodo factory para la conversion de json a dart

--- lib/features/map/data/models/categoria_residuos_dto.dart
+++ lib/features/map/data/models/categoria_residuos_dto.dart
@@ -8,4 +8,12 @@ class CategoriaResiduosDto {
     required this.categoria,
     required this.colorHex
   });
+
+  factory CategoriaResiduosDto.fromJson(Map<String, dynamic> json) {
+    return CategoriaResiduosDto(
+      idCategoriaResiduos: json['id_categoria_residuos'],
+      categoria: json['categoria'],
+      colorHex: json['color_hex']
+    );
+  }
 }
