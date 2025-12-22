d4788467b07c475b8a5d730b3e01f1690a365abd
Cristian <cristian.mg.david@gmail.com>
2025-12-22 12:58:59 -0300

    feat(categoria_residuos): crear nuevo archivo
    - Crear nueva entidad 'CategoriaResiduos' con sus propiedades id, categoria y color

---
+++ lib/features/map/domain/entities/categoria_residuos.dart
@@ -0,0 +1,11 @@
+class CategoriaResiduos {
+  final int idCategoriaResiduos;
+  final String categoria;
+  final String colorHex;
+
+  const CategoriaResiduos({
+    required this.idCategoriaResiduos,
+    required this.categoria,
+    required this.colorHex
+  });
+}
