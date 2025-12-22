e61c2c76837049bb324d5fa2210d63a927a1bb23
Cristian <cristian.mg.david@gmail.com>
2025-12-22 18:54:36 -0300

    feat(residuos_dto): cambiar constructor
    - Cambiar constructor para que sea requerible todas las propiedades

--- lib/features/map/data/models/residuos_dto.dart
+++ lib/features/map/data/models/residuos_dto.dart
@@ -1,19 +1,19 @@
 class ResiduosDto {
   final int idResiduo;
   final String nombre;
-  final String? imagen;
-  final double? peso;
-  final String? instruccionReciclado;
-  final String? descripcion;
+  final String imagen;
+  final double peso;
+  final String instruccionReciclado;
+  final String descripcion;
   final int idCategoriaResiduo;
 
-  ResiduosDto(
-    this.idResiduo,
-    this.nombre,
-    this.imagen,
-    this.peso,
-    this.instruccionReciclado,
-    this.descripcion,
-    this.idCategoriaResiduo,
+  ResiduosDto({
+    required this.idResiduo,
+    required this.nombre,
+    required this.imagen,
+    required this.peso,
+    required this.instruccionReciclado,
+    required this.descripcion,
+    required this.idCategoriaResiduo,}
   );
 }
