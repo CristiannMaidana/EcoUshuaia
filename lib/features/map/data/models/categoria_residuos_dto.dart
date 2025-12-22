47359fd06edb5fa363ed685b18e4a3c9edeb2b33
Cristian <cristian.mg.david@gmail.com>
2025-12-22 13:01:02 -0300

    feat(categoria_residuos_dto): crear nuevo archivo
    - Crear clase 'CategoriaResiduosDto', crear propiedades id, categor y color

---
+++ lib/features/map/data/models/categoria_residuos_dto.dart
@@ -0,0 +1,11 @@
+class CategoriaResiduosDto {
+  final int idCategoriaResiduos;
+  final String categoria;
+  final String colorHex;
+
+  const CategoriaResiduosDto({
+    required this.idCategoriaResiduos,
+    required this.categoria,
+    required this.colorHex
+  });
+}
