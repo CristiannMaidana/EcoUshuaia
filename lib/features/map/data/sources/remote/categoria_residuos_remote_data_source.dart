9a569df8cea864e2180db80a8be775e55361221a
Cristian <cristian.mg.david@gmail.com>
2025-12-22 13:23:05 -0300

    feat(categoria_residuos_remote_data_source): crear nuevo metodo
    - Crear metodo Future para devolver lista de todos los elementos de la tabla de la base de datos
    
    - Crear variable data para obtener los datos de la url de la tabla a obtener

--- lib/features/map/data/sources/remote/categoria_residuos_remote_data_source.dart
+++ lib/features/map/data/sources/remote/categoria_residuos_remote_data_source.dart
@@ -1,7 +1,14 @@
 import 'package:eco_ushuaia/core/network/http_client.dart';
+import 'package:eco_ushuaia/features/map/data/models/categoria_residuos_dto.dart';
 
 class CategoriaResiduosRemoteDataSource {
   final ApiClient api;
 
   CategoriaResiduosRemoteDataSource(this.api);
+
+  Future<List<CategoriaResiduosDto>> list ({Map<String, dynamic>? filtros}) async {
+    final data = await api.get('/categoria_residuos/', query: filtros);
+
+    return const <CategoriaResiduosDto>[];
+  }
 }
