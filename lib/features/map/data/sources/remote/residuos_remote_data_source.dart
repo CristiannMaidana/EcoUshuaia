ba12c01881e8fc92d183c368b389571e5165f8a3
Cristian <cristian.mg.david@gmail.com>
2025-12-22 19:10:25 -0300

    feat(residuos_remote_data_sources): crear metodo
    - Crear metodo list, crear variable data para obtener la lista de json con el metodo get
    
    - Comprobar los resultados de data si hay entrega una lista si no entrega una lista vacia

--- lib/features/map/data/sources/remote/residuos_remote_data_source.dart
+++ lib/features/map/data/sources/remote/residuos_remote_data_source.dart
@@ -1,7 +1,22 @@
 import 'package:eco_ushuaia/core/network/http_client.dart';
+import 'package:eco_ushuaia/features/map/data/models/residuos_dto.dart';
 
 class ResiduosRemoteDataSource {
   final ApiClient api;
 
   ResiduosRemoteDataSource(this.api);
+
+  Future<List<ResiduosDto>> list({Map<String, dynamic>? filtros}) async {
+    final data = await api.get('/residuos/', query: filtros);
+
+   List<dynamic> list;
+    if (data is Map && data['results'] is List) {
+      list = data['results'] as List;
+    } else if (data is List){
+      list = data;
+    } else {
+      list = const [];
+    }
+    return list.whereType<Map>().map((e) => ResiduosDto.fromJson(e as Map<String, dynamic>)).toList();
+  }
 }
