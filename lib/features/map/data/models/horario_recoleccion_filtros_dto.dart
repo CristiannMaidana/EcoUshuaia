5ca6faf5a369361bdc9ed0667f069c416cf7d876
Cristian <cristian.mg.david@gmail.com>
2025-12-27 00:09:07 -0300

    feat(horario_recoleccion_filtros_dto): crear metodo
    - Crear metodo 'factory' para mapear el json con la entidad dto

--- lib/features/map/data/models/horario_recoleccion_filtros_dto.dart
+++ lib/features/map/data/models/horario_recoleccion_filtros_dto.dart
@@ -1,5 +1,5 @@
 class HorarioRecoleccionFiltrosDto {
-    final int idCategoriaResiduos;
+  final int idCategoriaResiduos;
   final String? horaInicio;
   final int? diaSemana;
   final int? idZona;
@@ -10,4 +10,13 @@ class HorarioRecoleccionFiltrosDto {
     this.diaSemana,
     this.idZona
   });
+
+  factory HorarioRecoleccionFiltrosDto.fromJson(Map<String, dynamic> json){
+    return HorarioRecoleccionFiltrosDto(
+      idCategoriaResiduos: json['id_categoria_residuo'],
+      horaInicio: json['hora_inicio']!,
+      diaSemana: json['dia_semana']!,
+      idZona: json['id_zona']!,
+    );
+  }
 }
