87d92671144fc45b09442dbe61e5ea1c7f9ee9b5
Cristian <cristian.mg.david@gmail.com>
2025-12-27 13:32:03 -0300

    feat(horario_recoleccion_filtros_remote_data_sources): nuevos metodos
    - Crear metodo 'porHoraDiaZona', para obtencion de lista filtrada por url con las poriedades necesarias
    
    - Crear una variable _url de tipo String para almacenar la url base del backend
    
    - Crear metodo helper '_toDtoList' para obtener los datos y verificar en que forma vienen y devolver una lista mapeada con la clase Dto, reutilizable para todas las consultas por url

--- lib/features/map/data/sources/remote/horario_recoleccion_filtros_remote_data_sources.dart
+++ lib/features/map/data/sources/remote/horario_recoleccion_filtros_remote_data_sources.dart
@@ -1,7 +1,29 @@
 import 'package:eco_ushuaia/core/network/http_client.dart';
+import 'package:eco_ushuaia/features/map/data/models/horario_recoleccion_filtros_dto.dart';
 
 class HorarioRecoleccionFiltrosRemoteDataSources {
   final ApiClient api;
 
   HorarioRecoleccionFiltrosRemoteDataSources(this.api);
+
+  static const String _url = '/horario_recoleccion';
+
+  Future<List<HorarioRecoleccionFiltrosDto>> porHoraDiaZona({
+    required String hhmmss, required int dia, required int zona}) async {
+    final data = await api.get('$_url/horario_inicio/$hhmmss/dia_semana/$dia/zona_id/$zona');
+    return _toDtoList(data);
+  }
+
+  Future<List<HorarioRecoleccionFiltrosDto>> _toDtoList(dynamic data) async {
+    List<dynamic> list;
+    if (data is Map && data['results'] is List) {
+      list = data['results'] as List;
+    } else if (data is List){
+      list = data;
+    } else {
+      list = const [];
+    }
+
+    return list.whereType<Map>().map((e) => HorarioRecoleccionFiltrosDto.fromJson(e as Map<String, dynamic>)).toList();
+  }
 }
