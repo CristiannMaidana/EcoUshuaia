187a0bb474f7411e92bd925adf5b43593b721906
Cristian <cristian.mg.david@gmail.com>
2025-12-27 12:59:09 -0300

    feat(horario_recoleccion_filtros_remote_data_sources): nuevo archivo
    - Crear nueva clase 'HorarioRecoleccionFiltrosRemoteDataSources', para el manejo de obtencion de los archivos json por url

---
+++ lib/features/map/data/sources/remote/horario_recoleccion_filtros_remote_data_sources.dart
@@ -0,0 +1,7 @@
+import 'package:eco_ushuaia/core/network/http_client.dart';
+
+class HorarioRecoleccionFiltrosRemoteDataSources {
+  final ApiClient api;
+
+  HorarioRecoleccionFiltrosRemoteDataSources(this.api);
+}
