ea9f4e200e852d966568f3a0c4ceb4cc55e1bf4f
Cristian <cristian.mg.david@gmail.com>
2025-12-28 19:49:52 -0300

    feat(horario_recoleccion_filtros_repository): crear metodos
    - Crear metodo 'porHoraDiaZona' para la obtencion de url por hora, dia y zona
    
    - Crear metodo 'porDiaZona' para la obtencion de url por dia y zona
    
    - Crear metodo 'porHoraMannanaZona' para la obtencion de url por hora, dia y zona
    
    - Crear metodo 'semanaDesdeDiaZona', para la obtencion de url para todos los dias desde el dia actual
    
    - Todos los metodos obtienen lista en json de las consultas de la tabla HorarioRecoleccion

--- lib/features/map/domain/repositories/horario_recoleccion_filtros_repository.dart
+++ lib/features/map/domain/repositories/horario_recoleccion_filtros_repository.dart
@@ -1,5 +1,15 @@
 import 'package:eco_ushuaia/features/map/domain/entities/horario_recoleccion_filtros.dart';
 
-abstract class HorarioRecoleccionFiltrosRepository {
-  Future<List<HorarioRecoleccionFiltros>> list({Map<String, dynamic> filtros});
+abstract class HorarioRecoleccionFiltrosRepository {  
+  Future<List<HorarioRecoleccionFiltros>> porHoraDiaZona({
+    required String hhmmss, required int dia, required int zona});
+
+  Future<List<HorarioRecoleccionFiltros>> porDiaZona({
+    required int dia, required int zona});
+
+  Future<List<HorarioRecoleccionFiltros>> porHoraMannanaZona({
+    required String hhmmss, required int mannana, required int zona});
+
+  Future<List<HorarioRecoleccionFiltros>> semanaDesdeDiaZona({
+    required int desdeDia, required int zona});
 }
