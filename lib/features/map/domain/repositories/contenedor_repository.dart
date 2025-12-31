d4d6af502794cb67f3cb1aeeb6631abd8db9b978
Cristian <cristian.mg.david@gmail.com>
2025-12-31 15:49:00 -0300

    feat(contenedor_repository): crear metodo
    - Crear metodo 'filtrosResiduos', recive por parametro ids, para obtener una lista de contenedores filtrados por id de residuos

--- lib/features/map/domain/repositories/contenedor_repository.dart
+++ lib/features/map/domain/repositories/contenedor_repository.dart
@@ -2,4 +2,6 @@ import 'package:eco_ushuaia/features/map/domain/entities/contenedor.dart';
 
 abstract class ContenedorRepository {
   Future<List<Contenedor>> list({Map<String, dynamic>? filtros});
+
+  Future<List<Contenedor>> filtrosResiduos(List<int> ids);
 }
