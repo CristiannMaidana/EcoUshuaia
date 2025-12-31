4ce30bf2f93b082613ed8c83daf522b43d9f6c57
Cristian <cristian.mg.david@gmail.com>
2025-12-31 19:02:31 -0300

    feat(button_filter_viewmodel): cambiar tipo de map y metodos
    - En variable '_filtros' cambair el map a tipo 'dynamic', para obtener claves de tipo texto y numerico
    
    - Cambiar todos los metodos donde va la clave a tipo 'dynamic'
    
    - En metodo 'clean' agregar limpieza de filtros

--- lib/features/map/presentation/viewmodels/button_filter_viewmodel.dart
+++ lib/features/map/presentation/viewmodels/button_filter_viewmodel.dart
@@ -7,15 +7,15 @@ class ButtonFilterViewmodel extends ChangeNotifier{
   Set<String> get selected => _selected;
 
   // Map de ids de botones filtros
-  final Map<int, List<int>> _filtros = {};
-  Map<int, List<int>> get filtros => _filtros;
+  final Map<dynamic, List<int>> _filtros = {};
+  Map<dynamic, List<int>> get filtros => _filtros;
 
 
   // Obtener el valor boolean para cambiar el color del estado del boton
   bool isSelected(String boton) => _selected.contains(boton);
 
   // Metodo para agrega o quita el boton seleccionado y actualizar el estado
-  void toggle(String boton, int tipoBoton, int ids){
+  void toggle(String boton, dynamic tipoBoton, List<int> ids){
     if (_selected.contains(boton)){
       _selected.remove(boton);
       removeIdFilter(tipoBoton, ids);
@@ -30,21 +30,23 @@ class ButtonFilterViewmodel extends ChangeNotifier{
   // Metodo para limpiar todos los filtros y actualizar el estado
   void clean(){
     _selected.clear();
+    filtros.clear();
     notifyListeners();
   }
 
   // Metodo con id caracteristico de boton mas ids de filtros, agrega en un case de a 1 el add
-  void addIdsFilter(int tipoBoton, int ids){
-    if (tipoBoton >= 1 && tipoBoton <= 4) {
-      filtros.putIfAbsent(tipoBoton, () => []).add(ids);
-    }
+  void addIdsFilter(dynamic tipoBoton, List<int> ids){
+      for(int id in ids){
+        filtros.putIfAbsent(tipoBoton, () => []).add(id);
+      }
   }
 
   // Metodo para eliminar ids de botones seleccionados
-  void removeIdFilter(int tipoBoton, int idAEliminar) {
+  void removeIdFilter(dynamic tipoBoton, List<int> idAEliminar) {
     if (filtros.containsKey(tipoBoton)) {
-      
-      filtros[tipoBoton]?.remove(idAEliminar);
+      for(int id in idAEliminar){
+        filtros[tipoBoton]?.remove(id);
+      }
     }
   }
 }
