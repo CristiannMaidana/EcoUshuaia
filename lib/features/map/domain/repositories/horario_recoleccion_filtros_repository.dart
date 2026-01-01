52b4ab33f3a0c95ce2bba5a001d11b14d7c91488
Cristian <cristian.mg.david@gmail.com>
2026-01-01 17:13:50 -0300

    feat(horario_recoleccion_filtros_repository): eliminar metodo
    - Eliminar metodo abstracto 'porHoraMannanaZona' ya que se usara el metodo 'porDiaZona', solo se incrementara el valor al dia para que entregue los datos seleccionados correspondientes al dia siguiente

--- lib/features/map/domain/repositories/horario_recoleccion_filtros_repository.dart
+++ lib/features/map/domain/repositories/horario_recoleccion_filtros_repository.dart
@@ -7,9 +7,6 @@ abstract class HorarioRecoleccionFiltrosRepository {
   Future<List<HorarioRecoleccionFiltros>> porDiaZona({
     required int dia, required int zona});
 
-  Future<List<HorarioRecoleccionFiltros>> porHoraMannanaZona({
-    required String hhmmss, required int mannana, required int zona});
-
   Future<List<HorarioRecoleccionFiltros>> semanaDesdeDiaZona({
     required int desdeDia, required int zona});
 }
