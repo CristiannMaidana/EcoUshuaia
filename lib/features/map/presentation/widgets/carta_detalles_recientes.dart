a52494679b3e6a40a5fc2c70339e77e353243879
Cristian <cristian.mg.david@gmail.com>
2026-01-03 19:12:07 -0300

    feat(carta_detalles_recientes): crear widgets
    - Crear detalles de 'Container', borde y color
    
    - Crear Container con icon, que luego se cambiaran por las propiedades en el constructor
    
    - Crear column donde ira lo textos referidos al detalle del contenedor
    
    - Crear 'Row', con dos 'IconButton', donde esta la estrella de favoritos y para ir al contenedor en el mapa

--- lib/features/map/presentation/widgets/carta_detalles_recientes.dart
+++ lib/features/map/presentation/widgets/carta_detalles_recientes.dart
@@ -1,12 +1,77 @@
 import 'package:flutter/material.dart';
 
 class CartaDetallesRecientes extends StatelessWidget{
+  //TODO: crear propiedades, (entidad, para tener los textos, id, y favorito de usuario), obtener elementos de icons
   CartaDetallesRecientes({super.key});
 
   @override
   Widget build (BuildContext context){
     return Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.grey),
+        borderRadius: BorderRadius.circular(16) 
+      ),
+      padding: EdgeInsets.all(10),
+      child: Row(
+        children: [
+          // Icon del boton
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              border: Border.all(width: 1, color: Colors.grey),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            //TODO: cambia el icon en base a las propiedades del constructor
+            child: Icon(Icons.circle, size: 20,),
+          ),
 
+          SizedBox(width: 12),
+
+          //Texto de datos contenedor
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('nombre contenedor - residuo', textAlign: TextAlign.center),
+                  Text('distancia - Recoleccion: hoy', textAlign: TextAlign.center),
+                ],
+              ),
+            ),
+          ),
+          
+          // Botones para interactar con el contenedor
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                onPressed: (){}, 
+                icon: Icon(Icons.star, color: Colors.yellow.shade600),
+                style: IconButton.styleFrom(
+                  side: BorderSide(width: 1, color: Colors.grey),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: (){
+                  // TODO: enviar id y generar ruta para ir
+                }, 
+                icon: Icon(Icons.arrow_forward),
+                style: IconButton.styleFrom(
+                  side: BorderSide(width: 1, color: Colors.grey),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
