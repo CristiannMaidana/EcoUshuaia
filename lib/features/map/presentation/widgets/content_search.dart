f390e226657a39fdb00ec9cd13c40f326c3658e2
Cristian <cristian.mg.david@gmail.com>
2026-01-03 17:43:27 -0300

    feat(content_search): crear widget
    - Crear widget 'ContentSearch', mostrara el contenido que tiene de opciones de busqueda cuando abra el sheet para buscar direcciones
    
    - Crear hijo Column

---
+++ lib/features/map/presentation/widgets/content_search.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ContentSearch extends StatefulWidget{
+
+  const ContentSearch({super.key});
+
+  @override
+  State<ContentSearch> createState() => ContentSearchState();
+}
+
+class ContentSearchState extends State<ContentSearch>{
+  @override
+  Widget build (BuildContext context){
+    return Column();
+  }
+}
