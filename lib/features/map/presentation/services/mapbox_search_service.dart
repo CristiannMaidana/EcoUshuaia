2e3541b117b376c4280997f7072fe333cceb3ad9
Cristian <cristian.mg.david@gmail.com>
2026-01-04 19:07:28 -0300

    feat(mapbox_search_service): crear servicio
    - Crear clase 'MapboxSearchService' crear instancia de clase 'GeoCodingApi', con limite de resultados 5, respuestas en español y dos tipos de datos
    
    - Crear metodo 'search' para obtener desde el repo que nos provee el parquete mapbox_search, el resultado de la query, mapear el resultado con la entidad 'PlaceLocation' para entregarlo en tipo lista

---
+++ lib/features/map/presentation/services/mapbox_search_service.dart
@@ -0,0 +1,27 @@
+import 'package:eco_ushuaia/features/map/domain/entities/place_location.dart';
+import 'package:mapbox_search/mapbox_search.dart';
+
+class MapboxSearchService {
+  // Instancia de GeoCodingApi de mapbox_search
+  final GeoCodingApi _geo = GeoCodingApi(
+    limit: 5,
+    language: 'es',
+    types: const [PlaceType.address, PlaceType.place],
+  );
+
+  Future<List<PlaceLocation>> search(String query) async {
+    final resp = await _geo.getPlaces(query.trim());
+    return resp.fold(
+      (places) => places
+          .where((p) => p.center != null)
+          .map((p) => PlaceLocation(
+                lat: p.center!.lat,
+                lon: p.center!.long,
+                name: p.text,
+                address: p.placeName,
+              ))
+          .toList(),
+      (err) => throw Exception('Mapbox search error: ${err.message}'),
+    );
+  }
+}
