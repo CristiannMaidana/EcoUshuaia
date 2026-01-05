c3ab355839f989ac98a16b45294810158607395b
Cristian <cristian.mg.david@gmail.com>
2026-01-05 17:47:53 -0300

    feat(sheet_address): reconvertir widget
    - Convertir el widget en un 'StatefulWidget'
    
    - Crear esqueleto de 'DraggableScrollableSheet'
    
    - Crear controller, metodos de inico de controller
    
    - Crear barra de arrastre

--- lib/features/map/presentation/widgets/sheet_address.dart
+++ lib/features/map/presentation/widgets/sheet_address.dart
@@ -1,13 +1,82 @@
 import 'package:flutter/material.dart';
 
-class SheetAddress extends StatelessWidget {
-  // TODO: agregar propiedad, direccion, distanciad del usuario, zona, contenedores cercanos
-  SheetAddress({
+class SheetAddress extends StatefulWidget {
+  const SheetAddress({
     super.key,
+    this.initialChildSize = 0.5,
+    this.minChildSize = 0.2,
+    this.maxChildSize = 0.9,
   });
+
+  final double initialChildSize;
+  final double minChildSize;
+  final double maxChildSize;
+
+  @override
+  State<SheetAddress> createState() => _SheetAddressState();
+}
+
+class _SheetAddressState extends State<SheetAddress> {
+  late final DraggableScrollableController _draggableController;
+
+  @override
+  void initState() {
+    super.initState();
+    _draggableController = DraggableScrollableController();
+  }
+
+  @override
+  void dispose() {
+    _draggableController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack();
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        DraggableScrollableSheet(
+          controller: _draggableController,
+          initialChildSize: widget.initialChildSize,
+          minChildSize: widget.minChildSize,
+          maxChildSize: widget.maxChildSize,
+          builder: (context, scrollController) {
+            return SafeArea(
+              top: false,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
+                  border: Border.all(color: Colors.grey.shade500),
+                ),
+                child: ListView(
+                  controller: scrollController,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  padding: const EdgeInsets.all(16),
+                  children: [
+                    Column(
+                      children: [
+                        //Barra de arrastre
+                        Container(
+                          width: 70,
+                          height: 4,
+                          margin: const EdgeInsets.only(bottom: 16),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade400,
+                            borderRadius: BorderRadius.circular(2),
+                          ),
+                        ),
+                        // Contenido del sheet 
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ],
+    );
   }
 }
