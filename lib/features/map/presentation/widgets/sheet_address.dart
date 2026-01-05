85f98009cfa9dc30f9377da61b014b448bdc3753
Cristian <cristian.mg.david@gmail.com>
2026-01-05 17:28:15 -0300

    feat(sheet_address): crear widget
    - Crear widget 'SheetAddress', para mostrar datos de la direccion destino que el usuario quiere ir

---
+++ lib/features/map/presentation/widgets/sheet_address.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SheetAddress extends StatelessWidget {
+  // TODO: agregar propiedad, direccion, distanciad del usuario, zona, contenedores cercanos
+  SheetAddress({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack();
+  }
+}
