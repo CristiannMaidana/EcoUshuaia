0512fb7542d5277062eda39fc2bb61265ddfc283
Cristian <cristian.mg.david@gmail.com>
2026-03-15 17:50:15 -0300

    feat(header_for_addres_is_cloes): crear nuevo widget
    - Crear widget para mostrar contenido del header cuando esta minimizado el sheet
    
    - Agregarle el manejo del sheet por constructor con 'onVerticalDragUpdateFromFather' y 'onVerticalDragEndFromFather', mas la direccion que mostrara seleccionada y un boton para cancelar
    
    - Importar widget CircleIcon

---
+++ lib/features/map/presentation/widgets/header_for_addres_is_close.dart
@@ -0,0 +1,45 @@
+import 'package:eco_ushuaia/core/ui/widgets/barra_agarre.dart';
+import 'package:eco_ushuaia/features/calendar/presentation/widgets/circle_icon.dart';
+import 'package:flutter/material.dart';
+
+class HeaderForAddressIsClose extends StatelessWidget{
+  final GestureDragUpdateCallback onVerticalDragUpdateFromFather;
+  final GestureDragEndCallback onVerticalDragEndFromFather;
+  final String address;
+  final VoidCallback onPressedClose;
+
+  const HeaderForAddressIsClose({
+    super.key,
+    required this.onVerticalDragUpdateFromFather,
+    required this.onVerticalDragEndFromFather,
+    required this.address,
+    required this.onPressedClose,
+  });
+  
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onVerticalDragUpdate: onVerticalDragUpdateFromFather,
+      onVerticalDragEnd: onVerticalDragEndFromFather,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 27, vertical: 4.5),
+        child: Column(
+          children: [
+            BarraAgarre(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(address),
+                CircleIcon(
+                  icon: Icons.close,
+                  onPressed: onPressedClose,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
