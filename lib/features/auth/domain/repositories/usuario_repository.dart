18e0220993f4e35ef2dd5ad8b14a7bd695d3dfae
Cristian <cristian.mg.david@gmail.com>
2026-03-19 20:11:24 -0300

    feat(usuario_repository): agregar propiedad
    - Para enviar la contraseña a la hora de crear usuario

--- lib/features/auth/domain/repositories/usuario_repository.dart
+++ lib/features/auth/domain/repositories/usuario_repository.dart
@@ -1,12 +1,11 @@
 import 'package:eco_ushuaia/core/domain/entities/usuario.dart';
 
 abstract class UsuariosRepository {
-
   Future<Usuario> create({
     required String nombreUsuario,
     required String apellidoUsuario,
     required String email,
-    
+    required String password,
     int? idDireccion,
     int? idZona,
     int? idTipoUsuario,
