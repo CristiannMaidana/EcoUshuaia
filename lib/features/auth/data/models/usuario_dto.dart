8775586dacbf43cfc25ce3a035cd6290c2218bee
Cristian <cristian.mg.david@gmail.com>
2026-03-19 19:41:34 -0300

    feat(usuario_dto): reestructurar clase
    - Cambiar propiedad opcional
    
    - Dejar solo la conversion de entidad y el fromJson
    
    - Ya no hace post solo get

--- lib/features/auth/data/models/usuario_dto.dart
+++ lib/features/auth/data/models/usuario_dto.dart
@@ -1,17 +1,16 @@
 import 'package:eco_ushuaia/core/domain/entities/usuario.dart';
 
 class UsuarioDto {
-  final int? idUsuario;
+  final int idUsuario;
   final String nombreUsuario;
   final String apellidoUsuario;
   final String email;
-
   final int? idDireccion;
   final int? idZona;
   final int? idTipoUsuario;
 
   UsuarioDto({
-    this.idUsuario,
+    required this.idUsuario,
     required this.nombreUsuario,
     required this.apellidoUsuario,
     required this.email,
@@ -22,7 +21,7 @@ class UsuarioDto {
 
   factory UsuarioDto.fromJson(Map<String, dynamic> json) {
     return UsuarioDto(
-      idUsuario: json['id_usuario'] as int?,
+      idUsuario: json['id_usuario'] as int,
       nombreUsuario: json['nombre_usuario'] as String,
       apellidoUsuario: json['apellido_usuario'] as String,
       email: json['email'] as String,
@@ -32,44 +31,13 @@ class UsuarioDto {
     );
   }
 
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{
-      'nombre_usuario': nombreUsuario,
-      'apellido_usuario': apellidoUsuario,
-      'email': email,
-      'id_direccion': idDireccion,
-      'id_zona': idZona,
-      'id_tipo_usuario': idTipoUsuario,
-    };
-    map.removeWhere((k, v) => v == null);
-    return map;
-  }
-
   Usuario toEntity() => Usuario(
-        idUsuario: idUsuario ?? 0,
-        nombreUsuario: nombreUsuario,
-        apellidoUsuario: apellidoUsuario,
-        email: email,
-        idDireccion: idDireccion,
-        idZona: idZona,
-        idTipoUsuario: idTipoUsuario,
-      );
-
-  static UsuarioDto fromForm({
-    required String nombre,
-    required String apellido,
-    required String email,
-    
-    int? idDireccion,
-    int? idZona,
-    int? idTipoUsuario,
-  }) =>
-      UsuarioDto(
-        nombreUsuario: nombre,
-        apellidoUsuario: apellido,
-        email: email,
-        idDireccion: idDireccion,
-        idZona: idZona,
-        idTipoUsuario: idTipoUsuario,
-      );
+    idUsuario: idUsuario,
+    nombreUsuario: nombreUsuario,
+    apellidoUsuario: apellidoUsuario,
+    email: email,
+    idDireccion: idDireccion,
+    idZona: idZona,
+    idTipoUsuario: idTipoUsuario,
+  );
 }
