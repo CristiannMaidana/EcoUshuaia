12f54cc020fbc8d1a43e63228600da9e81c7080c
Cristian <cristian.mg.david@gmail.com>
2026-03-20 20:59:56 -0300

    feat(edit_user_screen): agregar body
    - Permitir arrastrar el contenido del body, agregarle margen y alinear contenidos

--- lib/features/settings/presentation/screens/edit_user_screen.dart
+++ lib/features/settings/presentation/screens/edit_user_screen.dart
@@ -1,4 +1,5 @@
 import 'package:eco_ushuaia/core/theme/colors.dart';
+import 'package:eco_ushuaia/features/settings/presentation/widgets/custom_card_option_settings.dart';
 import 'package:flutter/material.dart';
 
 class EditUserScreen extends StatefulWidget{
@@ -21,6 +22,15 @@ class _EditUserScreenState extends State<EditUserScreen> with SingleTickerProvid
         backgroundColor: camarone50,
         title: Text('Editar perfil', style: Theme.of(context).textTheme.headlineLarge),
         centerTitle: false,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsetsGeometry.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
       ),
     );
   }
