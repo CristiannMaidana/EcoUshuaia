4249c145a000509ef464743a3a1f2ae15b2d9a3a
Cristian <cristian.mg.david@gmail.com>
2026-03-19 22:44:39 -0300

    refactor(login_validatos): reestructurar metodo estatico a validacion por backend

--- lib/core/utils/validators/login_validators.dart
+++ lib/core/utils/validators/login_validators.dart
@@ -5,16 +5,7 @@ String? validarEmail(String? value) {
   if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
     return 'El email no es válido';
   }
-  if (!value.endsWith('@gmail.com') && !value.endsWith('@hotmail.com')) {
-    return 'El email debe ser gmail.com o hotmail.com';
-  }
-  else {
-    //Aca deberia traer todos los emails de la base de datos y comparar si el email ingresado ya existe
-    //if (value == 'Cristian@gmail.com'){
-    //  return true.toString();
-    //}
-    //else return false.toString();
-  }
+  return null;
 }
 
 String? emailNuevoValidator(String? value) {
@@ -28,18 +19,9 @@ String? emailNuevoValidator(String? value) {
   return null;
 }
 
-String? validarPassword(String? value){
-  if (value == null || value.isEmpty){
+String? validarPassword(String? value) {
+  if (value == null || value.isEmpty) {
     return 'La contraseña no puede estar vacío';
   }
-  else {
-    //Acá debería hacer la validacíon del user. y su contraseña, 
-    //Si coincide con contraseña del usuario.
-    //if (value == '123456'){
-    //  return true.toString();
-    //}
-    //else{
-    //  return false.toString();
-    //}
-  }
+  return null;
 }
