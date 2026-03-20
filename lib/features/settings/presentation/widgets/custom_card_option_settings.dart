870c82b1e6d3d562defeec1d316a4abd6c43676e
Cristian <cristian.mg.david@gmail.com>
2026-03-20 16:14:11 -0300

    feat(custom_card_option_settings): crear nuevo widget
    - Crear widget re utilizable de opciones de settings
    
    - Crear opcion de bordes dependiendo de tipo de variable bool
    
    - Texto y subtitulo para mostrar las opciones de configuracion del boton seleccionado
    
    - Opciones de botones de accion, cambiar segun switchWidget

---
+++ lib/features/settings/presentation/widgets/custom_card_option_settings.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:eco_ushuaia/core/ui/inputs/switch_toggle.dart';
+
+class CustomCardOptionSettings extends StatelessWidget {
+  final String titulo;
+  final String subtitulo;
+  final Widget icon;
+  final Widget? goIcon;
+  final Color color;
+  final bool switchWidget;
+  final bool? top;
+  final bool? bottom;
+  final bool? all;
+  final VoidCallback? actionSetting;
+  final ValueChanged<bool>? onToggle;
+
+  const CustomCardOptionSettings({
+    super.key,
+    required this.titulo,
+    required this.subtitulo,
+    required this.icon,
+    this.goIcon,
+    required this.color,
+    this.switchWidget = false,
+    this.top,
+    this.bottom,
+    this.all,
+    this.actionSetting,
+    this.onToggle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius:
+            all == true
+                ? BorderRadius.circular(24)
+                : BorderRadius.only(
+                  topLeft: top == true ? Radius.circular(24) : Radius.zero,
+                  topRight: top == true ? Radius.circular(24) : Radius.zero,
+                  bottomLeft: bottom == true ? Radius.circular(24) : Radius.zero,
+                  bottomRight: bottom == true ? Radius.circular(24) : Radius.zero,
+                ),
+        border: Border.all(width: 1, color: Colors.grey.withValues(alpha: 0.2)),
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // Icono dentro de un contenedor 
+            Container(
+              decoration: BoxDecoration(
+                color: color,
+                borderRadius: BorderRadius.circular(18),
+                border: Border.all(width: 1, color: color.withValues(alpha: 0.1)),
+              ),
+              padding: EdgeInsets.all(10),
+              child: icon,
+            ),
+        
+            // Texto con titulo y subtítulo
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(titulo, style: Theme.of(context).textTheme.labelLarge,),
+                SizedBox(height: 4),
+                Text(subtitulo, style: Theme.of(context).textTheme.bodySmall,),
+              ],
+            ),
+        
+            // Espacio para el botón de acción
+            switchWidget
+                ? SwitchToggle(onChanged: onToggle)
+                : IconButton(
+                    icon: goIcon!,
+                    onPressed: actionSetting,
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
