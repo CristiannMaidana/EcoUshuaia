b0b561f71551d62fff273e221e51d5abb28b2cb3
Cristian <cristian.mg.david@gmail.com>
2026-03-23 16:46:39 -0300

    feat(auth_usuario_dto): cambiar nombre a propiedad de clase

--- lib/features/auth/data/models/auth_usuario_dto.dart
+++ lib/features/auth/data/models/auth_usuario_dto.dart
@@ -1,14 +1,14 @@
 class AuthUsuarioDto {
-  final String email;
+  final String username;
   final String password;
 
   AuthUsuarioDto({
-    required this.email,
+    required this.username,
     required this.password,
   });
 
   Map<String, dynamic> toJson() => {
-    'email': email,
+    'username': username,
     'password': password,
   };
 }
