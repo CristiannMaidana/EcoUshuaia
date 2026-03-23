e80ce79db0beb1bda1c53d4a0deab3dfc83ea858
Cristian <cristian.mg.david@gmail.com>
2026-03-23 16:17:44 -0300

    feat(usuarios_create_remote_data_sources): cambiar nombre de archivo y clase

--- lib/features/auth/data/sources/remote/usuarios_create_remote_data_source.dart
+++ lib/features/auth/data/sources/remote/usuarios_create_remote_data_source.dart
@@ -2,10 +2,10 @@ import '../../../../../core/network/http_client.dart';
 import '../../models/usuario_create_request_dto.dart';
 import '../../models/usuario_dto.dart';
 
-class UsuariosRemoteDataSource {
+class UsuariosCreateRemoteDataSource {
   final ApiClient api;
 
-  UsuariosRemoteDataSource(this.api);
+  UsuariosCreateRemoteDataSource(this.api);
 
   Future<UsuarioDto> postUser(UsuarioCreateRequestDto dto) async {
     final data = await api.post('/usuarios/', body: dto.toJson(), requiresAuth: false);
