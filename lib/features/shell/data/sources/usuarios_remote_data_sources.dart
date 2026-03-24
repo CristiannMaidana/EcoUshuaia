7c6784694c2abfbd2d88d71d12c428aee76f082b
Cristian <cristian.mg.david@gmail.com>
2026-03-24 14:23:40 -0300

    feat(usuario_remote_data_sources): crear metodo
    - Crear metodo para actualizar los datos del usuario
    
    - Crear conexion con la url del backen, actualizar el nuevo nombre en la entidad usuario

--- lib/features/shell/data/sources/usuarios_remote_data_sources.dart
+++ lib/features/shell/data/sources/usuarios_remote_data_sources.dart
@@ -1,5 +1,6 @@
 import 'package:eco_ushuaia/core/network/http_client.dart';
 import 'package:eco_ushuaia/features/shell/data/models/usuario_dto.dart';
+import 'package:eco_ushuaia/features/shell/data/models/usuario_update_request_dto.dart';
 
 class UsuariosRemoteDataSources {
   final ApiClient api;
@@ -9,5 +10,23 @@ class UsuariosRemoteDataSources {
   Future<UsuarioDto> getUser() async {
     final data = await api.get('/usuarios/me/');
     return UsuarioDto.fromJson(data as Map<String, dynamic>);
+  }
+
+  Future<UsuarioDto> updateUser({required String nombreUsuario, required String apellidoUsuario}) async {
+    final dto = UsuarioUpdateRequestDto(
+      nombreUsuario: nombreUsuario,
+      apellidoUsuario: apellidoUsuario,
+    );
+    final data = await api.patch('/usuarios/me/', body: dto.toJson());
+
+    if (data is Map<String, dynamic>) {
+      return UsuarioDto.fromJson(data);
+    }
+
+    if (data is Map) {
+      return UsuarioDto.fromJson(Map<String, dynamic>.from(data));
+    }
+
+    return getUser();
   }
 }
