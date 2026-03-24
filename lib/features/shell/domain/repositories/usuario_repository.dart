3bd691c8b196129634fa094e3be387915ad0e69c
Cristian <cristian.mg.david@gmail.com>
2026-03-24 14:21:55 -0300

    feat(usuario_repository): crear metodo
    - Crear metodo para actualizar los datos del usuario (nombre y apellido )

--- lib/features/shell/domain/repositories/usuario_repository.dart
+++ lib/features/shell/domain/repositories/usuario_repository.dart
@@ -2,4 +2,6 @@ import 'package:eco_ushuaia/core/domain/entities/usuario.dart';
 
 abstract class UsuarioRepository {
   Future<Usuario> getUser();
+
+  Future<Usuario> updateUser({required String nombreUsuario, required String apellidoUsuario});
 }
