2e9f06ce38c8d1f21446f06319b6812223e012c3
Cristian <cristian.mg.david@gmail.com>
2026-03-24 14:49:24 -0300

    feat(usuario_update_request_dto): agregar propiedad
    - Agregar propiedad email
    
    - Agregar nueva propiedad a metodo toJson

--- lib/features/shell/data/models/usuario_update_request_dto.dart
+++ lib/features/shell/data/models/usuario_update_request_dto.dart
@@ -1,16 +1,19 @@
 class UsuarioUpdateRequestDto {
   final String? nombreUsuario;
   final String? apellidoUsuario;
+  final String? email;
 
   const UsuarioUpdateRequestDto({
     this.nombreUsuario,
     this.apellidoUsuario,
+    this.email,
   });
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{
       'nombre_usuario': nombreUsuario,
       'apellido_usuario': apellidoUsuario,
+      'email': email,
     };
     map.removeWhere((key, value) => value == null);
     return map;
