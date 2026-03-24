3b00f2fa4fa9e7061a11a1c07e00262b8790335a
Cristian <cristian.mg.david@gmail.com>
2026-03-24 15:13:42 -0300

    feat(usuario_update_request_dto): agregar propiedades a constructor y metodo

--- lib/features/shell/data/models/usuario_update_request_dto.dart
+++ lib/features/shell/data/models/usuario_update_request_dto.dart
@@ -2,11 +2,15 @@ class UsuarioUpdateRequestDto {
   final String? nombreUsuario;
   final String? apellidoUsuario;
   final String? email;
+  final String? currentPassword;
+  final String? password;
 
   const UsuarioUpdateRequestDto({
     this.nombreUsuario,
     this.apellidoUsuario,
     this.email,
+    this.currentPassword,
+    this.password,
   });
 
   Map<String, dynamic> toJson() {
@@ -14,6 +18,8 @@ class UsuarioUpdateRequestDto {
       'nombre_usuario': nombreUsuario,
       'apellido_usuario': apellidoUsuario,
       'email': email,
+      'current_password': currentPassword,
+      'password': password,
     };
     map.removeWhere((key, value) => value == null);
     return map;
