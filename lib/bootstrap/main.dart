59a198a0c4d2ce71709e786449ae7c3c651db8b5
Cristian <cristian.mg.david@gmail.com>
2026-03-30 23:54:23 -0300

    feat(main): agregar servicio
    - Importar service
    
    - Agregar services a la ejecucion de aplicacion de inicio

--- lib/bootstrap/main.dart
+++ lib/bootstrap/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:eco_ushuaia/app/app.dart';
+import 'package:eco_ushuaia/core/services/mapbox_initializer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  MapboxInitializer.ensureInitialized();
   runApp(const App());
 }
