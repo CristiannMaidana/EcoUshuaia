b5d73a28f06264983cd3797f4643729355d77ed0
Cristian <cristian.mg.david@gmail.com>
2026-04-01 23:49:13 -0300

    feat(usuario_contenedor_favoritos): crear nueva entidad

---
+++ lib/features/map/domain/entities/usuario_contenedor_favoritos.dart
@@ -0,0 +1,11 @@
+class UsuarioContenedorFavoritos {
+  final String notaUsuarioContenedor;
+  final int idContenedor;
+  final int idUsuario;
+
+  UsuarioContenedorFavoritos({
+    required this.notaUsuarioContenedor,
+    required this.idContenedor,
+    required this.idUsuario,
+  });
+}
