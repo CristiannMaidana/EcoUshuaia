291c40b2c733920f4b96af84d5b68820892bcdff
Cristian <cristian.mg.david@gmail.com>
2026-04-06 19:54:19 -0300

    feat(content_search): agregar provider y agregar funcionalidad
    - Importar widgets
    
    - Agregar provider
    
    - Agregar funcionalidad para casos de falla de carga de provider
    
    - Generar constructor de contenedores favoritos

--- lib/features/map/presentation/widgets/content_search.dart
+++ lib/features/map/presentation/widgets/content_search.dart
@@ -1,18 +1,22 @@
+import 'package:eco_ushuaia/features/map/presentation/viewmodels/usuario_contenedores_favoritos_viewmodel.dart';
 import 'package:eco_ushuaia/features/map/presentation/widgets/carta_detalles_recientes.dart';
 import 'package:eco_ushuaia/features/map/presentation/widgets/expansion_tile_custom.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class ContentSearch extends StatefulWidget{
-
+class ContentSearch extends StatefulWidget {
   const ContentSearch({super.key});
 
   @override
   State<ContentSearch> createState() => ContentSearchState();
 }
 
-class ContentSearchState extends State<ContentSearch>{
+class ContentSearchState extends State<ContentSearch> {
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
+    final vmFavoritos = context.watch<UsuarioContenedoresFavoritosViewModel>();
+    final favoritos = vmFavoritos.favoritos;
+
     return Column(
       children: [
         // Seccion de favoritos
@@ -20,11 +24,26 @@ class ContentSearchState extends State<ContentSearch>{
           padding: EdgeInsetsGeometry.symmetric(vertical: 10, horizontal: 10),
           child: ExpansionTileCustom(
             title: 'Favoritos',
-            // TODO: generador de contenedores favoritos, en base a la base de datos
-            child: CartaDetallesRecientes(),
             initiallyOpen: true,
+            child: favoritos.isEmpty
+                ? Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text('No hay contenedores favoritos guardados'),
+                  )
+                : Column(
+                    children: favoritos
+                        .map(
+                          (contenedor) => Padding(
+                            padding: EdgeInsets.only(bottom: 10),
+                            child: CartaDetallesRecientes(
+                              contenedor: contenedor,
+                            ),
+                          ),
+                        )
+                        .toList(growable: false),
+                  ),
           ),
-        )
+        ),
       ],
     );
   }
