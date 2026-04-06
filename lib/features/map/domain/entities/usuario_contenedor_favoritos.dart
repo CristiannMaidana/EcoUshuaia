346d37969dadeb429db84e93902f28f80c0f7278
Cristian <cristian.mg.david@gmail.com>
2026-04-05 23:32:37 -0300

    feat(usuario_contenedor_favoritos): cambiar requerimiento de variables

--- lib/features/map/domain/entities/usuario_contenedor_favoritos.dart
+++ lib/features/map/domain/entities/usuario_contenedor_favoritos.dart
@@ -1,10 +1,12 @@
 class UsuarioContenedorFavoritos {
-  final String notaUsuarioContenedor;
+  final int? idUsuarioRegistroContenedor;
+  final String? notaUsuarioContenedor;
   final int idContenedor;
   final int idUsuario;
 
-  UsuarioContenedorFavoritos({
-    required this.notaUsuarioContenedor,
+  const UsuarioContenedorFavoritos({
+    this.idUsuarioRegistroContenedor,
+    this.notaUsuarioContenedor,
     required this.idContenedor,
     required this.idUsuario,
   });
