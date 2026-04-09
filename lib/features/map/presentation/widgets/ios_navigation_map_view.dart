f4ce8fec453c8dfaa7d5fdd70c738ce708610f30
Cristian <cristian.mg.david@gmail.com>
2026-04-09 19:48:08 -0300

    feat(ios_navigation_map_view): cambiar estado de widget
    - Agregar canal
    
    - Agregar metodos

--- lib/features/map/presentation/widgets/ios_navigation_map_view.dart
+++ lib/features/map/presentation/widgets/ios_navigation_map_view.dart
@@ -2,17 +2,37 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class IosNavigationMapView extends StatelessWidget {
+class IosNavigationMapView extends StatefulWidget {
   final double latitude;
   final double longitude;
   final String? title;
+  final ValueChanged<Map<String, dynamic>>? onRouteInfoChanged;
 
   const IosNavigationMapView({
     super.key,
     required this.latitude,
     required this.longitude,
     this.title,
+    this.onRouteInfoChanged,
   });
+
+  @override
+  State<IosNavigationMapView> createState() => _IosNavigationMapViewState();
+}
+
+class _IosNavigationMapViewState extends State<IosNavigationMapView> {
+  MethodChannel? _channel;
+
+  void _onPlatformViewCreated(int id) {
+    _channel = MethodChannel('eco_ushuaia/navigation_map_view/$id');
+
+    _channel!.setMethodCallHandler((call) async {
+      if (call.method == 'onRouteInfoChanged') {
+        final data = Map<String, dynamic>.from(call.arguments as Map);
+        widget.onRouteInfoChanged?.call(data);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +43,12 @@ class IosNavigationMapView extends StatelessWidget {
     return UiKitView(
       viewType: 'eco_ushuaia/navigation_map_view',
       creationParams: {
-        'latitude': latitude,
-        'longitude': longitude,
-        'title': title,
+        'latitude': widget.latitude,
+        'longitude': widget.longitude,
+        'title': widget.title,
       },
       creationParamsCodec: const StandardMessageCodec(),
+      onPlatformViewCreated: _onPlatformViewCreated,
     );
   }
 }
