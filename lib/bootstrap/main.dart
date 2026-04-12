5dbddb0e4ea34ffcf3fdb8a41834586240ff8f65
Cristian <cristian.mg.david@gmail.com>
2026-04-12 17:14:03 -0300

    feat(main): eliminar importacion de widget
    - Elimminar validacion de mapbox de dependencia flutter

--- lib/bootstrap/main.dart
+++ lib/bootstrap/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:eco_ushuaia/app/app.dart';
-import 'package:eco_ushuaia/core/services/mapbox_initializer.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  MapboxInitializer.ensureInitialized();
   runApp(const App());
 }
