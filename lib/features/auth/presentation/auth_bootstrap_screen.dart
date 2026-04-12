3e35ee493008d0b0c815c28bffdcf4dd704a1cdc
Cristian <cristian.mg.david@gmail.com>
2026-04-12 17:11:35 -0300

    feat(auth_bootstrap_screen): cambiar estado de widget
    - Agregar metodo init para restaurar sesion de usuario si todavia tiene tiempo en token

--- lib/features/auth/presentation/auth_bootstrap_screen.dart
+++ lib/features/auth/presentation/auth_bootstrap_screen.dart
@@ -4,13 +4,26 @@ import 'package:eco_ushuaia/features/shell/presentation/app_shell_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class AuthBootstrapScreen extends StatelessWidget {
+class AuthBootstrapScreen extends StatefulWidget {
   const AuthBootstrapScreen({super.key});
+
+  @override
+  State<AuthBootstrapScreen> createState() => _AuthBootstrapScreenState();
+}
+
+class _AuthBootstrapScreenState extends State<AuthBootstrapScreen> {
+  late final Future<bool> _restoreSessionFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _restoreSessionFuture = context.read<AuthUsuarioRepository>().restoreSession();
+  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<bool>(
-      future: context.read<AuthUsuarioRepository>().restoreSession(),
+      future: _restoreSessionFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState != ConnectionState.done) {
           return const Scaffold(
