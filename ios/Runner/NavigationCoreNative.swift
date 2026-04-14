225dafe1f664a8f39214940aa671af3340720d55
Cristian <cristian.mg.david@gmail.com>
2026-04-14 19:04:34 -0300

    feat(NavigationCoreNative): implementar generador de preview de ruta

--- ios/Runner/NavigationCoreNative.swift
+++ ios/Runner/NavigationCoreNative.swift
@@ -1,5 +1,29 @@
+import CoreLocation
 import Foundation
+import MapboxNavigationCore
 
 final class NavigationCoreNative {
-    // Vacía por ahora.
+    private let navigationProvider: MapboxNavigationProvider
+
+    init(navigationProvider: MapboxNavigationProvider) {
+        self.navigationProvider = navigationProvider
+    }
+
+    @MainActor func buildPreviewRoute(
+        origin: CLLocationCoordinate2D,
+        destination: CLLocationCoordinate2D,
+        completion: @escaping (Result<NavigationRoutes, Error>) -> Void
+    ) {
+        let options = NavigationRouteOptions(coordinates: [origin, destination])
+        let request = navigationProvider.routingProvider().calculateRoutes(options: options)
+
+        Task {
+            switch await request.result {
+            case .failure(let error):
+                completion(.failure(error))
+            case .success(let navigationRoutes):
+                completion(.success(navigationRoutes))
+            }
+        }
+    }
 }
