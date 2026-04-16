2310fc198d5af2d147b1b7b8fecb3c8b74d601d5
Cristian <cristian.mg.david@gmail.com>
2026-04-16 18:25:42 -0300

    feat(address_turn_by_turn): crear widget turn-by-turn
    - Crear widget para mostrar los datos de direccion en el recorrido

---
+++ lib/features/map/presentation/widgets/address_turn_by_turn.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AddressTurnByTurn extends StatefulWidget{
+
+  const AddressTurnByTurn({
+    super.key,
+  });
+
+  @override
+  State<AddressTurnByTurn> createState() => _AddressTurnByTurnState();
+}
+
+class _AddressTurnByTurnState extends State<AddressTurnByTurn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
