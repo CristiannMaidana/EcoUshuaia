dbc18f4d3a7cd5d21905fa1ae3d23df0822ccb1d
Cristian <cristian.mg.david@gmail.com>
2026-04-16 16:49:04 -0300

    docs(search_bar): agregar implementacion TODO

--- lib/features/map/presentation/widgets/search_bar.dart
+++ lib/features/map/presentation/widgets/search_bar.dart
@@ -78,6 +78,7 @@ class SerchBarState extends State<SerchBar> with SingleTickerProviderStateMixin{
                   }
                   vm.clearSuggestions(); // Limpiar sugerencias al enviar
                   widget.cerrar(); // Cerrar el sheet
+                  //TODO: enviar direccion seleccionada al mapa para mostrar pin y ruta, y eliminar resto de pines, solo contenedores, cargandolo en metodo 
                   widget.detalleDireccion(); // Abrir detalle de direccion
                 },
               ),
