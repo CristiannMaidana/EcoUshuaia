441a8708dbce97bb16979042003e937a0fc71543
Cristian <cristian.mg.david@gmail.com>
2026-04-16 18:39:57 -0300

    feat(address_turn_by_turn): agregar contenido a container y propiedades

--- lib/features/map/presentation/widgets/address_turn_by_turn.dart
+++ lib/features/map/presentation/widgets/address_turn_by_turn.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AddressTurnByTurn extends StatefulWidget{
-
-  const AddressTurnByTurn({
-    super.key,
-  });
+class AddressTurnByTurn extends StatefulWidget {
+  const AddressTurnByTurn({super.key});
 
   @override
   State<AddressTurnByTurn> createState() => _AddressTurnByTurnState();
@@ -14,12 +11,40 @@ class _AddressTurnByTurnState extends State<AddressTurnByTurn> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
       height: 100,
+      padding: const EdgeInsets.all(12),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(36),
         border: Border.all(color: Colors.black12, width: 1),
-        boxShadow: const [BoxShadow(blurRadius: 12, color: Colors.black12, offset: Offset(0, 4))],
+        boxShadow: const [
+          BoxShadow(
+            blurRadius: 12,
+            color: Colors.black12,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(Icons.directions, size: 70, color: Colors.blueAccent[400]),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Giro a la derecha en Av. San Martín',
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              const SizedBox(height: 8),
+              Text('A 200 metros',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
