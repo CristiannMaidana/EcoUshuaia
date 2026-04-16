2e5a7a961df4f477b7d3cdafea1d8ac0d5f3f124
Cristian <cristian.mg.david@gmail.com>
2026-04-16 18:31:05 -0300

    feat(address_turn_by_turn): agregar decoracion a container

--- lib/features/map/presentation/widgets/address_turn_by_turn.dart
+++ lib/features/map/presentation/widgets/address_turn_by_turn.dart
@@ -14,7 +14,13 @@ class _AddressTurnByTurnState extends State<AddressTurnByTurn> {
   @override
   Widget build(BuildContext context) {
     return Container(
-
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(36),
+        border: Border.all(color: Colors.black12, width: 1),
+        boxShadow: const [BoxShadow(blurRadius: 12, color: Colors.black12, offset: Offset(0, 4))],
+      ),
     );
   }
 }
