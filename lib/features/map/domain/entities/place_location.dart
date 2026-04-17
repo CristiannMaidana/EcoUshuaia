9d1f2a2a9dda0a86eef2b05bf78c60b1081f80dd
Cristian <cristian.mg.david@gmail.com>
2026-04-17 13:36:27 -0300

    feat(place_location): agregar funcionalidad para buscar direcciones
    - Agregar propiedad en entidad
    
    - Agregar metodo get

--- lib/features/map/domain/entities/place_location.dart
+++ lib/features/map/domain/entities/place_location.dart
@@ -3,11 +3,15 @@ class PlaceLocation {
   final double lon;
   final String? name;
   final String? address;
+  final String? suggestionId;
 
   const PlaceLocation({
-    required this.lat, 
-    required this.lon, 
-    this.name, 
-    this.address
+    required this.lat,
+    required this.lon,
+    this.name,
+    this.address,
+    this.suggestionId,
   });
+
+  bool get isSuggestion => suggestionId != null;
 }
