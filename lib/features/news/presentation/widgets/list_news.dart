0b17f3f5bbdd2d61e94a8b2bade015bd869b7927
Cristian <cristian.mg.david@gmail.com>
2026-06-01 18:06:29 -0300

    feat(list_news): add button for go up when scroll

--- lib/features/news/presentation/widgets/list_news.dart
+++ lib/features/news/presentation/widgets/list_news.dart
@@ -5,10 +5,49 @@ import 'package:eco_ushuaia/features/news/presentation/widgets/items_novedades.d
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class CustomNovedades extends StatelessWidget {
+class CustomNovedades extends StatefulWidget {
   final ValueChanged<Calendarios> expand;
 
   const CustomNovedades({super.key, required this.expand});
+
+  @override
+  State<CustomNovedades> createState() => _CustomNovedadesState();
+}
+
+class _CustomNovedadesState extends State<CustomNovedades> {
+  late final ScrollController _scrollController;
+  bool _showJumpTop = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController()..addListener(_onScrollChange);
+  }
+
+  @override
+  void dispose() {
+    _scrollController
+      ..removeListener(_onScrollChange)
+      ..dispose();
+    super.dispose();
+  }
+
+  void _onScrollChange() {
+    final show = _scrollController.offset > 100;
+    if (show != _showJumpTop) {
+      setState(() => _showJumpTop = show);
+    }
+  }
+
+  Future<void> _jumpToTop() async {
+    try {
+      await _scrollController.animateTo(
+        0,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    } catch (_) {}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +68,34 @@ class CustomNovedades extends StatelessWidget {
     final bool hasContentForSelectedDay =
         selectedDay == null || data.isNotEmpty;
 
-    return ClipRRect(
-      child: ListView(
-        padding: const EdgeInsets.only(top: 12),
-        children: [
-          if (hasContentForSelectedDay)
-            ItemsNovedades(listaNovedades: data, expand: expand),
-        ],
-      ),
+    return Stack(
+      children: [
+        ClipRRect(
+          child: ListView(
+            controller: _scrollController,
+            padding: const EdgeInsets.only(top: 12, bottom: 88),
+            children: [
+              if (hasContentForSelectedDay)
+                ItemsNovedades(listaNovedades: data, expand: widget.expand),
+            ],
+          ),
+        ),
+        if (_showJumpTop)
+          Positioned(
+            right: 16,
+            bottom: 16,
+            child: FloatingActionButton(
+              backgroundColor: const Color.fromRGBO(0, 0, 0, 0.8),
+              mini: true,
+              onPressed: _jumpToTop,
+              child: const Icon(
+                Icons.keyboard_arrow_up,
+                color: Colors.white,
+                size: 32,
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
