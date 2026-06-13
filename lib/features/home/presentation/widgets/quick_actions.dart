6ab2184111c4fc4b18cb3883e7786387cb1aca25
Cristian <cristian.mg.david@gmail.com>
2026-06-13 17:16:37 -0300

    refactor(quick_actions): change padding

--- lib/features/home/presentation/widgets/quick_actions.dart
+++ lib/features/home/presentation/widgets/quick_actions.dart
@@ -39,7 +39,7 @@ class QuickActions extends StatelessWidget {
     ];
 
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15),
+      padding: const EdgeInsets.only(left: 15),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
