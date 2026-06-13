27c02cd0b227e54bc2ca6f3a3dd12876c8e36602
Cristian <cristian.mg.david@gmail.com>
2026-06-13 16:42:15 -0300

    feat(home_screen): add viewmodels
    - Add providers
    
    - Initilizer the widget

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:eco_ushuaia/core/theme/colors.dart';
+import 'package:eco_ushuaia/features/calendar/domain/repositories/categoria_noticias_repositories.dart';
 import 'package:eco_ushuaia/features/calendar/presentation/viewmodels/calendario_viewmodel.dart';
+import 'package:eco_ushuaia/features/calendar/presentation/viewmodels/categoria_noticias_viewmodel.dart';
 import 'package:eco_ushuaia/core/ui/animations/notification_lottie.dart';
 import 'package:eco_ushuaia/features/home/presentation/widgets/custom_contenedores_usuario.dart';
 import 'package:eco_ushuaia/features/home/presentation/widgets/day_news.dart';
@@ -26,39 +28,42 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   Widget build(context){
     final calendarioVm = context.watch<CalendarioViewmodel>();
 
-    return Scaffold(
-      backgroundColor: camarone50,
-      appBar: AppBar(
+    return ChangeNotifierProvider(
+      create: (context) => CategoriaNoticiasViewmodel(context.read<CategoriaNoticiasRepositories>())..load(),
+      child: Scaffold(
         backgroundColor: camarone50,
-        title: const Text('Eco Ushuaia', style: TextStyle(color: Colors.black)),
-        actions: [
-          Container(
-            padding: EdgeInsets.only(right: 20),
-            child:  Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.white,
-                border: Border.all(
-                  width: 1
-                )
-              ),
-              child: NotificationLottie(notifications: listaNotificaciones)
-              ),
-          )
-        ],
-      ),    
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            QuickActions(),
-            CustomContenedoresUsuario(),
-            DayNews(news: calendarioVm.eventsOf(DateTime.now())),
-            CustomMaterialesInfo(),
-            CustomNovedadesHome(),
+        appBar: AppBar(
+          backgroundColor: camarone50,
+          title: const Text('Eco Ushuaia', style: TextStyle(color: Colors.black)),
+          actions: [
+            Container(
+              padding: EdgeInsets.only(right: 20),
+              child:  Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.white,
+                  border: Border.all(
+                    width: 1
+                  )
+                ),
+                child: NotificationLottie(notifications: listaNotificaciones)
+                ),
+            )
           ],
-        ),
-      )
+        ),    
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              QuickActions(),
+              CustomContenedoresUsuario(),
+              DayNews(news: calendarioVm.eventsOf(DateTime.now())),
+              CustomMaterialesInfo(),
+              CustomNovedadesHome(news: calendarioVm.eventsFromDay(DateTime.now())),
+            ],
+          ),
+        )
+      ),
     );
   }
 }
