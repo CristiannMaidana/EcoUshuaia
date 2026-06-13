86d4cfeba3337d0df60ad93fdd2e4d42deca15ec
Cristian <cristian.mg.david@gmail.com>
2026-06-13 16:47:45 -0300

    feat(new_news_item): refactor widget to a GestureDetector

--- lib/features/news/presentation/widgets/new_news_item.dart
+++ lib/features/news/presentation/widgets/new_news_item.dart
@@ -36,46 +36,51 @@ class CustomNewNews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(36),
-        border: Border.all(width: 1.5, color: Colors.grey[400]!),
-      ),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Row(
-            children: [
-              Icon(Icons.circle, size: 20, color: color),
-              SizedBox(width: 20),
-              // Texts and header
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //Header
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(titulo, style: Theme.of(context).textTheme.titleMedium),
-                        Text(_daysLeftText(fecha), style: Theme.of(context).textTheme.titleMedium),
-                      ],
-                    ),
-                    // Text
-                    Text(subtitulo ?? '',
-                      style: Theme.of(context).textTheme.bodySmall,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    Text(infoText, style: Theme.of(context).textTheme.bodySmall),
-                  ],
+    return GestureDetector(
+      onTap: (){
+        
+      },
+      child: Container(
+        height: 80,
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(36),
+          border: Border.all(width: 1.5, color: Colors.grey[400]!),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Row(
+              children: [
+                Icon(Icons.circle, size: 20, color: color),
+                SizedBox(width: 20),
+                // Texts and header
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Header
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(titulo, style: Theme.of(context).textTheme.titleMedium),
+                          Text(_daysLeftText(fecha), style: Theme.of(context).textTheme.titleMedium),
+                        ],
+                      ),
+                      // Text
+                      Text(subtitulo ?? '',
+                        style: Theme.of(context).textTheme.bodySmall,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Text(infoText, style: Theme.of(context).textTheme.bodySmall),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
