c13aa003b4930f31c42cd693b91600dd517e3b79
Cristian <cristian.mg.david@gmail.com>
2026-06-14 11:48:32 -0300

    feat(frequent_materials): new widget
    - Create section for the most frequent materials
    
    - Import widgets
    
    - Create structure of widget

---
+++ lib/features/waste_instructions/presentation/widgets/frequent_materials.dart
@@ -0,0 +1,60 @@
+import 'package:eco_ushuaia/features/home/presentation/widgets/card_touch.dart';
+import 'package:eco_ushuaia/features/waste_instructions/presentation/widgets/card_dynamic.dart';
+import 'package:flutter/material.dart';
+
+class FrequentMaterials extends StatelessWidget{
+
+  const FrequentMaterials({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CardDynamic(
+      widget: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Materiales frecuentes', style: Theme.of(context).textTheme.titleMedium,),
+                  Text('Accesos rápidos a los residuos más comunes.', style: Theme.of(context).textTheme.bodySmall,)
+                ],
+              ),
+              TextButton(
+                onPressed: (){}, 
+                child: Text('Ver todos')
+              )
+            ],
+          ),
+
+          // Buttons
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 110,
+                  child: CardTouch(title: 'Plastico', 
+                    infoText: 'Envases, botellas y recipientes.', 
+                    width: 250
+                  ),
+                ),
+                SizedBox(width: 10,),
+                CardTouch(title: 'Papel y cartón', 
+                  infoText: 'Papel, cajas y cartones reciclables en buen estado', 
+                  width: 250
+                ),
+                SizedBox(width: 10,),
+                CardTouch(title: 'Vidrio', 
+                  infoText: 'Botellas y frascos sin contenido ni tapas.', 
+                  width: 250
+                ),
+              ],
+            )
+          ),
+        ],
+      )
+    );
+  }
+}
