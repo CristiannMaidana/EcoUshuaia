6b2663178a895899065c05fd8a0df9bb17ea3743
Cristian <cristian.mg.david@gmail.com>
2026-06-18 17:55:05 -0300

    feat(app_theme): comment text and add style for button

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -6,18 +6,19 @@ final ThemeData appTheme = ThemeData(
   scaffoldBackgroundColor: camarone300,
   appBarTheme: const AppBarTheme(backgroundColor: camarone300),
 
-  textTheme: TextTheme(
-    displayLarge: logo,
-    headlineLarge: subTitulo,
-    headlineMedium: titulo,
-    bodyLarge: cuerpo,
-    labelLarge: labelConTexto,
-    labelMedium: labelInput,
-    labelSmall: labelDeError,
-  ),
+  // textTheme: TextTheme(
+  //   displayLarge: logo,
+  //   headlineLarge: subTitulo,
+  //   headlineMedium: titulo,
+  //   bodyLarge: cuerpo,
+  //   labelLarge: labelConTexto,
+  //   labelMedium: labelInput,
+  //   labelSmall: labelDeError,
+  // ),
 
   inputDecorationTheme: appInputDecorationTheme,
 
   elevatedButtonTheme: ElevatedButtonThemeData(style: botonEstandar),
+  outlinedButtonTheme: OutlinedButtonThemeData(style: buttonSecundary),
 
 );
