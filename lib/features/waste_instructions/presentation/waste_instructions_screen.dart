341b86f80e405200c1079ecea89164d5b7e5b4a1
Cristian <cristian.mg.david@gmail.com>
2026-06-18 18:59:07 -0300

    refactor(waste_instructions_screen): delete background color

--- lib/features/waste_instructions/presentation/waste_instructions_screen.dart
+++ lib/features/waste_instructions/presentation/waste_instructions_screen.dart
@@ -1,22 +1,16 @@
-import 'package:eco_ushuaia/core/theme/colors.dart';
 import 'package:eco_ushuaia/features/waste_instructions/presentation/screens/materials_screen.dart';
 import 'package:eco_ushuaia/features/waste_instructions/presentation/widgets/frequent_materials.dart';
 import 'package:eco_ushuaia/features/waste_instructions/presentation/widgets/learn_waste.dart';
 import 'package:eco_ushuaia/features/waste_instructions/presentation/widgets/useful_access.dart';
 import 'package:flutter/material.dart';
 
-class WasteInstructionsScreen extends StatelessWidget{
-
+class WasteInstructionsScreen extends StatelessWidget {
   const WasteInstructionsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: camarone50,
-      appBar: AppBar(
-        backgroundColor: camarone50,
-        title: Text('EcoUshuaia'),
-      ),
+      appBar: AppBar(title: Text('EcoUshuaia')),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsetsGeometry.symmetric(horizontal: 15),
@@ -25,10 +19,8 @@ class WasteInstructionsScreen extends StatelessWidget{
               LearnWaste(
                 goMaterials: () {
                   Navigator.push(
-                    context, 
-                    MaterialPageRoute(
-                      builder: (_) => MaterialsScreen()
-                    )
+                    context,
+                    MaterialPageRoute(builder: (_) => MaterialsScreen()),
                   );
                 },
               ),
