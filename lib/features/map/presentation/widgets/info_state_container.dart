f1c7844e350ee5c916d6bb29cf09c2d72f9fa493
Cristian <cristian.mg.david@gmail.com>
2026-06-29 16:35:52 -0300

    feat(info_state_container): import color, change style, and add propiertie

--- lib/features/map/presentation/widgets/info_state_container.dart
+++ lib/features/map/presentation/widgets/info_state_container.dart
@@ -1,13 +1,16 @@
+import 'package:eco_ushuaia/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class InfoStateContainer extends StatelessWidget{
   final String titulo;
   final String descripcion;
+  final IconData icon;
 
   const InfoStateContainer({
     super.key, 
     required this.titulo,
     required this.descripcion,
+    required this.icon,
   });
 
   @override
@@ -15,7 +18,7 @@ class InfoStateContainer extends StatelessWidget{
     return Container(
       height: 80,
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey[400]!, width: 1),
+        border: Border.all(color: Colors.grey[400]!, width: .3),
         borderRadius: BorderRadius.circular(16),
       ),
       padding: EdgeInsets.symmetric(horizontal: 10),
@@ -23,7 +26,13 @@ class InfoStateContainer extends StatelessWidget{
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(titulo, style: Theme.of(context).textTheme.labelMedium),
+          Row(
+            children: [
+              Icon(icon, color: camarone600),
+              SizedBox(width: 5),
+              Text(titulo, style: Theme.of(context).textTheme.labelMedium),
+            ],
+          ),
           SizedBox(height: 8),
           Text(descripcion, style: Theme.of(context).textTheme.labelMedium),
         ],
