18057fa3895e81d89144f9222e0db8d3d7ba9f76
Cristian <cristian.mg.david@gmail.com>
2026-07-02 19:42:45 -0300

    refactor(switch_toggle): change color and style of button

--- lib/core/ui/inputs/switch_toggle.dart
+++ lib/core/ui/inputs/switch_toggle.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 
 class SwitchToggle extends StatefulWidget {
   final ValueChanged<bool>? onChanged;
+  final bool? value;
 
-  const SwitchToggle({super.key, this.onChanged});
+  const SwitchToggle({super.key, this.onChanged, this.value});
 
   @override
   State<SwitchToggle> createState() => _SwitchToggleState();
@@ -15,16 +16,22 @@ class _SwitchToggleState extends State<SwitchToggle> {
 
   @override
   Widget build(BuildContext context) {
+    final currentValue = widget.value ?? habilitado;
+
     return Switch(
-      value: habilitado,
+      value: currentValue,
       activeThumbColor: camarone600,
-      activeTrackColor: camarone300,
+      activeTrackColor: camarone200,
       inactiveThumbColor: camarone600,
       inactiveTrackColor: Colors.white,
+      trackOutlineColor: const WidgetStatePropertyAll(camarone700),
+      trackOutlineWidth: const WidgetStatePropertyAll(1.3),
       onChanged: (bool value) {
-        setState(() {
-          habilitado = value;
-        });
+        if (widget.value == null) {
+          setState(() {
+            habilitado = value;
+          });
+        }
         widget.onChanged?.call(value);
       },
     );
