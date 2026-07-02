f9559d3df58d11e21d6f7abae6b6e2da69a4c602
Cristian <cristian.mg.david@gmail.com>
2026-07-02 20:39:20 -0300

    refactor(learn_waste): change widget

--- lib/features/waste_instructions/presentation/widgets/learn_waste.dart
+++ lib/features/waste_instructions/presentation/widgets/learn_waste.dart
@@ -1,70 +1,61 @@
 import 'dart:async';
 
-import 'package:eco_ushuaia/core/theme/theme.dart';
-import 'package:eco_ushuaia/features/home/presentation/widgets/card_touch.dart';
-import 'package:eco_ushuaia/features/waste_instructions/presentation/widgets/card_dynamic.dart';
+import 'package:eco_ushuaia/features/settings/presentation/widgets/custom_card_option_settings.dart';
 import 'package:flutter/material.dart';
 
-class LearnWaste extends StatelessWidget{
+class LearnWaste extends StatelessWidget {
   final FutureOr<void> Function()? goMaterials;
 
-  const LearnWaste({
-    super.key,
-    this.goMaterials,
-  });
+  const LearnWaste({super.key, this.goMaterials});
 
   @override
   Widget build(BuildContext context) {
-    return CardDynamic(
-      widget: Column(
+    return Column(
         children: [
-          // Text
-          Container(
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: camarone100,
-              borderRadius: BorderRadius.circular(36),
-              border: Border.all(width: 1.5, color: Colors.grey[400]!),    
+          CustomCardOptionSettings(
+            titulo: 'Como separar',
+            subtitulo: 'Consejos generales y errores comunes.',
+            icon: const Icon(
+              Icons.checklist_rounded,
+              size: 25,
+              color: Color(0xFF237655),
             ),
-            child: Column(
-              children: [
-                Text('Aprendé a reciclar mejor', style: Theme.of(context).textTheme.titleMedium),
-                SizedBox(height: 10,),
-                Text('Entrá a una guía simple para ver cómo preparar cada residuo, qué  evitar y cuándo hace falta un punto especial.', style: Theme.of(context).textTheme.bodyMedium),
-              ],
-            ),
+            goIcon: const Icon(Icons.arrow_forward_ios_outlined, size: 15),
+            color: const Color(0xFFE5F5ED),
+            actionSetting: () {},
+            top: true,
+            switchWidget: false,
           ),
-          SizedBox(height: 10,),
-          // Card with actions
-          CardTouch(
-            title: 'Como separar', 
-            infoText: 'Consejos generales y errores comunes.', 
-            width: 400,
-            icon: Icons.checklist_rounded,
-            iconBackgroundColor: const Color(0xFFE5F5ED),
-            iconColor: const Color(0xFF237655),
-          ),              
-          CardTouch(
-            title: 'Ver materiales', 
-            infoText: 'Plástico, vidrio, papel y más.', 
-            width: 400,
-            icon: Icons.category_rounded,
-            iconBackgroundColor: const Color(0xFFE5F5ED),
-            iconColor: const Color(0xFF237655),
-            onTap: () async {
+          CustomCardOptionSettings(
+            titulo: 'Ver materiales',
+            subtitulo: 'Plástico, vidrio, papel y más.',
+            icon: const Icon(
+              Icons.category_rounded,
+              size: 25,
+              color: Color(0xFF237655),
+            ),
+            goIcon: const Icon(Icons.arrow_forward_ios_outlined, size: 15),
+            color: const Color(0xFFE5F5ED),
+            actionSetting: () async {
               await goMaterials?.call();
             },
+            switchWidget: false,
           ),
-          CardTouch(
-            title: 'Residuos especiales', 
-            infoText: 'Electrónicos, peligrosos y puntos especiales.', 
-            width: 400,
-            icon: Icons.error_outline_rounded,
-            iconBackgroundColor: const Color(0xFFE5F5ED),
-            iconColor: const Color(0xFF237655),
+          CustomCardOptionSettings(
+            titulo: 'Residuos especiales',
+            subtitulo: 'Electrónicos, peligrosos y puntos especiales.',
+            icon: const Icon(
+              Icons.error_outline_rounded,
+              size: 25,
+              color: Color(0xFF237655),
+            ),
+            goIcon: const Icon(Icons.arrow_forward_ios_outlined, size: 15),
+            color: const Color(0xFFE5F5ED),
+            actionSetting: () {},
+            bottom: true,
+            switchWidget: false,
           ),
         ],
-      ),
     );
   }
 }
