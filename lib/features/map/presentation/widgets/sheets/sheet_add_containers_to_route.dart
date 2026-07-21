d20e8614ef7c847de6f49a57ad30926d453c76e0
Cristian <cristian.mg.david@gmail.com>
2026-07-21 16:17:55 -0300

    feat(sheet_add_containers_to_route): create new widget

---
+++ lib/features/map/presentation/widgets/sheets/sheet_add_containers_to_route.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SheetAddContainersToRoute extends StatefulWidget {
+  const SheetAddContainersToRoute({
+    super.key,
+  });
+
+  @override
+  State<SheetAddContainersToRoute> createState() => SheetAddContainersToRouteState();
+}
+
+class SheetAddContainersToRouteState extends State<SheetAddContainersToRoute> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      
+    );
+  }
+}
