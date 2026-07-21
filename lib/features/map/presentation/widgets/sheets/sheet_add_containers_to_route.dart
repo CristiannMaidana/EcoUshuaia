3a70810acea6b8ca74d15610d99e0e7f61b2ff24
Cristian <cristian.mg.david@gmail.com>
2026-07-21 16:19:39 -0300

    feat(sheet_add_container_to_route): add propierties to constructor

--- lib/features/map/presentation/widgets/sheets/sheet_add_containers_to_route.dart
+++ lib/features/map/presentation/widgets/sheets/sheet_add_containers_to_route.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
 class SheetAddContainersToRoute extends StatefulWidget {
+  final double initialSheetSize;
+  final double minSheetSize;
+  final double maxSheetSize;
+
   const SheetAddContainersToRoute({
     super.key,
+    this.initialSheetSize = 0.00,
+    this.minSheetSize = 0.00,
+    this.maxSheetSize = 0.70,
   });
 
   @override
