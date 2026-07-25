cb15edb0d8feacb6605848769785e510de966a83
Cristian <cristian.mg.david@gmail.com>
2026-07-25 20:02:54 -0300

    refactor(showDialogPassword): reestructure widget

--- lib/features/auth/presentation/widgets/showDialogPassword.dart
+++ lib/features/auth/presentation/widgets/showDialogPassword.dart
@@ -1,16 +1,17 @@
+// ignore: file_names
 import 'package:eco_ushuaia/core/theme/colors.dart';
 import 'package:eco_ushuaia/core/ui/text/text_list_item.dart';
 import 'package:flutter/material.dart';
 
 class Showdialogpassword extends StatelessWidget{
-  Showdialogpassword({Key? key}): super (key : key);
+  const Showdialogpassword({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       backgroundColor: camarone300,
       title: Text('Instrucciones', style: TextStyle(color: camarone950),),
-      content: Container(
+      content: SizedBox(
         height: 120,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -18,10 +19,10 @@ class Showdialogpassword extends StatelessWidget{
           children: [
             Text('La contraseña debe tener al menos:', style: TextStyle(color: camarone950)),
             SizedBox(height: 8),
-            TextListItem('8 caracteres'),
-            TextListItem('1 mayúscula'),
-            TextListItem('1 número'),
-            TextListItem('1 caracter especial')
+            TextListItem(texto:'8 caracteres'),
+            TextListItem(texto: '1 mayúscula'),
+            TextListItem(texto: '1 número'),
+            TextListItem(texto: '1 caracter especial')
           ],
         ),
       ),
@@ -31,6 +32,6 @@ class Showdialogpassword extends StatelessWidget{
           child: Text('Aceptar', style: TextStyle(color: colorNegro)),
         ),
       ],
-    );                                
+    );
   }
 }
