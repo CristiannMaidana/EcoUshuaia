a75921ce7ed2f8f5b22884d7d636ca16ba848949
Cristian <cristian.mg.david@gmail.com>
2026-07-26 13:21:16 -0300

    refactor(set_address_screen): add propiertie for the keyboard on the widgets

--- lib/features/auth/presentation/screens/set_address_screen.dart
+++ lib/features/auth/presentation/screens/set_address_screen.dart
@@ -7,6 +7,7 @@ class SetAddressScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('Ingresar dirección'),
       ),
