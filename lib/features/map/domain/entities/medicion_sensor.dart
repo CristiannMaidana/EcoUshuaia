16c5d0b2bbff5945e895c6237439fb4f90b828d0
Cristian <cristian.mg.david@gmail.com>
2026-07-31 16:20:45 -0300

    feat(medicion_sensor): create entitie

---
+++ lib/features/map/domain/entities/medicion_sensor.dart
@@ -0,0 +1,11 @@
+class MedicionSensor {
+  final int idContenedor;
+  final int volumenMedido;
+  final String nivelLlenado;
+
+  const MedicionSensor({
+    required this.idContenedor,
+    required this.volumenMedido,
+    required this.nivelLlenado,
+  });
+}
