9498ab09698ea5e9a99103f9b5784ec92056b447
Cristian <cristian.mg.david@gmail.com>
2026-08-05 20:35:30 -0300

    feat(sheet_of_edit_reminder): add buttons

--- lib/features/calendar/presentation/widgets/sheets/sheet_of_edit_reminder.dart
+++ lib/features/calendar/presentation/widgets/sheets/sheet_of_edit_reminder.dart
@@ -1,7 +1,10 @@
+import 'package:eco_ushuaia/core/theme/colors.dart';
 import 'package:eco_ushuaia/core/ui/widgets/barra_agarre.dart';
 import 'package:eco_ushuaia/core/ui/widgets/sheet_generic.dart';
 import 'package:eco_ushuaia/features/calendar/presentation/widgets/circle_icon.dart';
+import 'package:eco_ushuaia/features/calendar/presentation/widgets/widgets_of_reminder/button_icon_show_text.dart';
 import 'package:eco_ushuaia/features/calendar/presentation/widgets/widgets_of_reminder/description_of_reminder.dart';
+import 'package:eco_ushuaia/features/calendar/presentation/widgets/widgets_of_reminder/open_sheet_tile_custom.dart';
 import 'package:eco_ushuaia/features/calendar/presentation/widgets/widgets_of_reminder/title_of_reminder.dart';
 import 'package:eco_ushuaia/features/calendar/presentation/widgets/widgets_of_reminder/wheel_picker_of_time.dart';
 import 'package:eco_ushuaia/features/map/presentation/widgets/expansion_tile_custom.dart';
@@ -20,8 +23,11 @@ class SheetOfEditReminder extends SheetGeneric {
 }
 
 class SheetOfEditReminderState extends SheetGenericState<SheetOfEditReminder> {
-  final WheelPickerOfTimeController _wheelPickerOfTimeController =
-      WheelPickerOfTimeController();
+  final WheelPickerOfTimeController _wheelPickerOfTimeController = WheelPickerOfTimeController();
+  bool _isReminderButtonSelected = false;
+  bool _isReminderButtonSelected1 = false;
+  bool _isReminderButtonSelected2 = false;
+  bool _isReminderButtonSelected3 = false;
 
   @override
   Widget headerOfSheet(BuildContext context) {
@@ -69,7 +75,64 @@ class SheetOfEditReminderState extends SheetGenericState<SheetOfEditReminder> {
           padding: const EdgeInsets.symmetric(horizontal: 10),
           child: Column(
             children: [
-              
+              // TODO: chango to allow only one selected button at a time, for default open the first button selected
+              Row(
+                children: [
+                  ButtonIconShowText(
+                    icon: Icons.insert_drive_file_outlined,
+                    text: 'Elegir fecha',
+                    isSelected: _isReminderButtonSelected,
+                    onSelected: () async {
+                      setState(() {
+                        _isReminderButtonSelected = !_isReminderButtonSelected;
+                      });
+                    },
+                    selectedColor: camarone600,
+                  ),
+                  const SizedBox(width: 10),
+
+                  ButtonIconShowText(
+                    icon: Icons.local_shipping,
+                    text: 'Recolección',
+                    isSelected: _isReminderButtonSelected1,
+                    onSelected: () async {
+                      setState(() {
+                        _isReminderButtonSelected1 = !_isReminderButtonSelected1;
+                      });
+                    },
+                    selectedColor: Color.fromRGBO(121, 83, 255, 1),
+                  ),
+                  const SizedBox(width: 10),
+               
+                  ButtonIconShowText(
+                    icon: Icons.delete,
+                    text: 'Llenado',
+                    isSelected: _isReminderButtonSelected2,
+                    onSelected: () async {
+                      setState(() {
+                        _isReminderButtonSelected2 = !_isReminderButtonSelected2;
+                      });
+                    },
+                    selectedColor: Color.fromRGBO(255, 189, 74, 1),
+                  ),
+                  const SizedBox(width: 10),
+
+                  ButtonIconShowText(
+                    icon: Icons.recycling,
+                    text: 'Disponible',
+                    isSelected: _isReminderButtonSelected3,
+                    onSelected: () async {
+                      setState(() {
+                        _isReminderButtonSelected3 = !_isReminderButtonSelected3;
+                      });
+                    },
+                    selectedColor: Color.fromRGBO(88, 172, 255, 1),
+                  ),
+                  const SizedBox(width: 10),
+                ],
+              ),
+              const SizedBox(height: 16),
+
               TitleOfReminder(
                 controller: TitleOfReminderController(),
                 onTitleChanged: (title) {
@@ -95,7 +158,7 @@ class SheetOfEditReminderState extends SheetGenericState<SheetOfEditReminder> {
                   
                   // TODO: in title have to be a var with the date selected, for default is today
                   ExpansionTileCustom(
-                    title: 'Fecha y hora',
+                    title: 'Selecciona fecha y hora',
                     initiallyOpen: false,
                     child: WheelPickerOfTime(
                       controller: _wheelPickerOfTimeController,
